23462a69d46347b8157a5397e5bff6908ff98221
Tim Conkling <tconkling@gmail.com>
2018-04-23 18:12:40 -0700

    pixi.js hello world

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 import * as PIXI from 'pixi.js';
 
-let app: PIXI.Application = new PIXI.Application({width: 512, height: 512});
+let app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
 document.body.appendChild(app.view);
 
-app.renderer.backgroundColor = 0x061639;
+// create a new Sprite from an image path
+let bunny = PIXI.Sprite.fromImage('assets/bunny.png');
+
+// center the sprite's anchor point
+bunny.anchor.set(0.5);
+
+// move the sprite to the center of the screen
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+
+app.stage.addChild(bunny);
+
+// Listen for animate update
+app.ticker.add(function(delta) {
+    // just for fun, let's rotate mr rabbit a little
+    // delta is 1 if running at 100% performance
+    // creates frame-independent transformation
+    bunny.rotation += 0.1 * delta;
+});
