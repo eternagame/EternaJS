4cea1818c2498696284b74daa8be429f927fd487
Tim Conkling <tconkling@gmail.com>
2018-05-03 15:11:34 -0700

    More emscripten testing

--- lib/emscripten_test/build.sh
+++ lib/emscripten_test/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-emcc -O2 --bind hello_world.cpp -o hello_world.js -s 'EXPORT_NAME="HelloWorld"' -s MODULARIZE=1
+emcc -O3 --bind hello_world.cpp -o hello_world.js -s 'EXPORT_NAME="HelloWorld"' -s MODULARIZE=1
--- lib/emscripten_test/hello_world.cpp
+++ lib/emscripten_test/hello_world.cpp
@@ -1,6 +1,10 @@
 
 #include <string>
 #include <math.h>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <emscripten.h>
 #include <emscripten/bind.h>
 
 using namespace emscripten;
@@ -8,24 +12,65 @@ using namespace emscripten;
 struct MyStruct {
     float floatVal;
     std::string stringVal;
+    std::vector<float> floatVec;
 };
 
-float do_sqrt(float x) {
-    return sqrt(x);
+struct MyClass {
+    float floatVal;
+    std::string stringVal;
+    std::vector<float> floatVec;
+};
+
+void TraceJS (const char* text) {
+    // EM_ASM_({
+    //     Module.print("(Emscripten) " + $0);
+    // }, text);
+
+    // EM_ASM_ is more performant, but passing strings around is a pain
+
+    std::string output = "Module.print('(Emscripten) " + std::string(text) + "');";
+    emscripten_run_script(output.c_str());
+}
+
+int GetStringLength (const std::string& str) {
+    return str.size();
 }
 
 MyStruct GetStruct() {
-    MyStruct myStruct;
-    myStruct.floatVal = 3.14f;
-    myStruct.stringVal = "Hello from MyStruct!";
-    return myStruct;
+    return MyStruct { 3.14f, "Hello from MyStruct!", { 0.33f, 0.66f } };
+}
+
+MyClass* GetStructPtr () {
+    return new MyClass { 666, "Hello from MyClass!", { 0.1f, 0.2f, 0.3f } };
+}
+
+void SendLog () {
+    TraceJS("Hello from C++!");
+}
+
+int main() {
+    std::cout << GetStruct().stringVal;
+    return 0;
 }
 
 EMSCRIPTEN_BINDINGS(hello_world) {
+    register_vector<float>("VectorFloat");
+
     value_object<MyStruct>("MyStruct")
         .field("floatVal", &MyStruct::floatVal)
-        .field("stringVal", &MyStruct::stringVal);
+        .field("stringVal", &MyStruct::stringVal)
+        .field("floatVec", &MyStruct::floatVec);
 
-    function("do_sqrt", &do_sqrt);
+    class_<MyClass>("MyClass")
+        .constructor()
+        .property("floatVal", &MyClass::floatVal)
+        .property("stringVal", &MyClass::stringVal)
+        .property("floatVec", &MyClass::floatVec);
+
+    function("SendLog", &SendLog);
     function("GetStruct", &GetStruct);
+    function("GetStringLength", &GetStringLength);
+
+    // GetStructPtr returns a raw pointer, so we need to use allow_raw_pointers()
+    function("GetStructPtr", &GetStructPtr, allow_raw_pointers());
 }
