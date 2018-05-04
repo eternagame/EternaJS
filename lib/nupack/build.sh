6eed9f353687cda837f1ddec23da0986a751a758
Tim Conkling <tconkling@gmail.com>
2018-05-03 18:15:48 -0700

    begin on nupack/EmscriptenBridge

---
+++ lib/nupack/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+em++ -O3 --bind -s 'EXPORT_NAME="nupack"' -s MODULARIZE=1 --std=c++11 \
+    -o nupack.js \
+    backtrack.c \
+    CalculateEnergy.c \
+    DNAGlobals.c \
+    EmscriptenBridge.cpp \
+    ene.c \
+    GetEnergy.c \
+    hash.c \
+    init.c \
+    mfeUtils.c \
+    min.c \
+    mt19937ar.c \
+    nsStar.c \
+    pairsPr.c \
+    pf.c \
+    pfuncUtils.c \
+    pknots.c \
+    ReadCommandLineNPK.c \
+    sumexp.c \
+    utils.c
