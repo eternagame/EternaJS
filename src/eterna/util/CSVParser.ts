e0fdf56c8d9ca8f50928d88d87901f14738b7b79
Tim Conkling <tconkling@gmail.com>
2018-05-15 15:57:26 -0700

    util.CSVParser

---
+++ src/eterna/util/CSVParser.ts
@@ -0,0 +1,42 @@
+﻿export class CSVParser {
+    public static parse_into_array(csl: string): string[] {
+        let vals: string[] = [];
+        let last_comma: number = -1;
+        let ii: number;
+
+        for (ii = 0; ii < csl.length; ii++) {
+            if (csl.charAt(ii) == ",") {
+                vals.push(csl.substr(last_comma + 1, ii - (last_comma + 1)));
+                last_comma = ii;
+            }
+        }
+
+        if (last_comma < ii) {
+            vals.push(csl.substr(last_comma + 1, ii - (last_comma + 1)));
+        }
+
+        return vals;
+    }
+
+    public static parse_into_array_with_white_spaces(csl: string): string[] {
+        let vals: string[] = [];
+        let last_comma: number = -1;
+        let ii: number;
+
+        for (ii = 0; ii < csl.length; ii++) {
+            if (csl.charAt(ii) == " ") {
+                if (last_comma < ii - 1) {
+                    vals.push(csl.substr(last_comma + 1, ii - (last_comma + 1)));
+                    last_comma = ii;
+                }
+            }
+        }
+
+        if (last_comma < ii - 1) {
+            vals.push(csl.substr(last_comma + 1, ii - (last_comma + 1)));
+        }
+
+        return vals;
+    }
+
+}
