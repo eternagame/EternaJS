e271e68a2534ce79ac3968a21a53bef03592cc9c
Tim Conkling <tconkling@gmail.com>
2018-05-15 18:34:16 -0700

    begin porting eterna/folding

---
+++ src/eterna/folding/Folder.ts
@@ -0,0 +1,150 @@
+﻿export abstract class Folder {
+    protected constructor(loaded_cb: () => void = null) {
+        this._on_loaded_cb = loaded_cb;
+        this._functional = false;
+    }
+
+    public is_functional(): boolean {
+        return this._functional;
+    }
+
+    public get_folder_name(): string {
+        return "";
+    }
+
+    public get_cache(key: Object): any {
+        let key_str = JSON.stringify(key);
+        return this._cache.get(key_str);
+    }
+
+    public can_score_structures(): boolean {
+        return false;
+    }
+
+    public score_structures(seq: number[], pairs: number[], temp: number = 37, nodes: any[] = null): number {
+        return 0;
+    }
+
+    public fold_sequence(seq: number[], second_best_pairs: number[], desired_pairs: string = null, temp: number = 37): any[] {
+        return null;
+    }
+
+    public fold_sequence_with_binding_site(seq: number[], target_pairs: number[], binding_site: any[], bonus: number, version: number = 2.0, temp: number = 37): any[] {
+        return null;
+    }
+
+    public can_cofold(): boolean {
+        return false;
+    }
+
+    public cofold_sequence(seq: number[], second_best_pairs: number[], malus: number = 0, desired_pairs: string = null, temp: number = 37): any[] {
+        return null;
+    }
+
+    public can_cofold_with_binding_site(): boolean {
+        return false;
+    }
+
+    /// Overridables
+
+    public cofold_sequence_with_binding_site(seq: number[], binding_site: any[], bonus: number, desired_pairs: string = null, malus: number = 0, temp: number = 37): any[] {
+        return null;
+    }
+
+    public can_dot_plot(): boolean {
+        return false;
+    }
+
+    public get_dot_plot(seq: number[], pairs: number[], temp: number = 37): any[] {
+        return null;
+    }
+
+    public load_custom_params(): boolean {
+        return false;
+    }
+
+    public can_multifold(): boolean {
+        return false;
+    }
+
+    public multifold(seq: number[], second_best_pairs: number[], oligos: any[], desired_pairs: string = null, temp: number = 37): Object {
+        return null;
+    }
+
+    public multifold_unroll(seq: number[], second_best_pairs: number[], oligos: any[], desired_pairs: string = null, temp: number = 37): any[] {
+        return null;
+    }
+
+    // public load_parameters_from_buffer(buf: ByteArray, done_cb: Function = null): boolean {
+    //     let res: boolean = false;
+    //     if (this._clib_inst != null) {
+    //         trace("supplying custom.par");
+    //         this._clib_inst.supplyFile("custom.par", buf);
+    //         res = this.load_custom_params();
+    //     }
+    //     return res;
+    // }
+    //
+    // public load_parameters_from_url(url: string, done_cb: Function = null): void {
+    //
+    //     if (this._clib_inst != null) {
+    //         let cb: Function = function (e: Event): void {
+    //             let res: boolean = false;
+    //             if (e.type != IOErrorEvent.IO_ERROR) {
+    //                 res = this.load_parameters_from_buffer(e.target.data);
+    //             }
+    //             if (done_cb != null) done_cb(res);
+    //         };
+    //
+    //         let rsc: URLLoader = new URLLoader();
+    //         rsc.dataFormat = URLLoaderDataFormat.BINARY;
+    //         rsc.addEventListener(IOErrorEvent.IO_ERROR, function (e: Event): void {
+    //             cb(e);
+    //         });
+    //         rsc.addEventListener(Event.COMPLETE, function (e: Event): void {
+    //             cb(e);
+    //         });
+    //         rsc.load(new URLRequest(url));
+    //         return;
+    //     }
+    //     if (done_cb != null) done_cb(false);
+    // }
+
+    public hairpin_energy(size: number, type: number, si1: number, sj1: number, sequence: any[], i: number, j: number): number {
+        return 0;
+    }
+
+    public loop_energy(n1: number, n2: number, type: number, type_2: number, si1: number, sj1: number, sp1: number, sq1: number, b1: boolean, b2: boolean): number {
+        return 0;
+    }
+
+    public cut_in_loop(i: number): number {
+        return 0;
+    }
+
+    public ml_energy(pairs: number[], S: any[], i: number, is_extloop: boolean): number {
+        return 0;
+    }
+
+    protected put_cache(key: Object, data: any): void {
+        let key_str = JSON.stringify(key);
+        this._cache.set(key_str, data);
+    }
+
+    /// Helpers
+
+    protected reset_cache(): void {
+        this._cache.clear();
+    }
+
+    protected on_loaded(): void {
+        if (this._on_loaded_cb != null) {
+            this._on_loaded_cb();
+        }
+    }
+
+    protected _functional: boolean;
+
+    private readonly _cache: Map<string, any> = new Map<string, any>();
+    private readonly _on_loaded_cb: Function;
+}
---
+++ src/eterna/folding/FolderManager.ts
@@ -0,0 +1,74 @@
+﻿import {Folder} from "./Folder";
+import {RNAFoldBasic} from "./RNAFoldBasic";
+import {Vienna} from "./Vienna";
+
+export class FolderManager {
+    public static get instance(): FolderManager {
+        if (FolderManager._instance == null) {
+            FolderManager._instance = new FolderManager;
+        }
+        return FolderManager._instance;
+    }
+
+    public add_folder(folder: Folder): void {
+        for (let other of this._folders) {
+            if (other.get_folder_name() == folder.get_folder_name()) {
+                throw new Error("Trying to generate folders with duplicate names ('" + folder.get_folder_name() + "')");
+            }
+        }
+        this._folders.push(folder);
+    }
+
+    public get_folder(name: string): Folder {
+        let folder: Folder;
+        for (folder of this._folders) {
+            if (folder.get_folder_name() == name) {
+                return folder;
+            }
+        }
+
+        throw new Error("Trying to query non-existing folder ('" + folder.get_folder_name() + "')");
+    }
+
+    public get_next_folder(folder_name: string, filter_cb: Function = null): Folder {
+        let ii: number;
+        for (ii = 0; ii < this._folders.length; ii++) {
+            if (this._folders[ii].get_folder_name() == folder_name) {
+                break;
+            }
+        }
+
+        if (ii >= this._folders.length) {
+            throw new Error("Folder '" + folder_name + "' doesn't exist");
+        }
+
+        let jj: number;
+        for (jj = 1; jj < this._folders.length; jj++) {
+            let idx: number = (ii + jj) % this._folders.length;
+            let folder: Folder = this._folders[idx];
+
+            if (folder.get_folder_name().length == 0 ||
+                folder.get_folder_name() == RNAFoldBasic.NAME ||
+                !folder.is_functional() ||
+                (filter_cb != null && filter_cb(folder))) {
+                continue;
+            }
+
+            AutosaveManager.instance.saveObjects([folder.get_folder_name()], "folder-" + Application.instance.get_player_id());
+            return folder;
+        }
+
+        return this._folders[ii]; // use same one
+    }
+
+    public get_last_used_folder(): string {
+        let pref: any[] = AutosaveManager.instance.loadObjects("folder-" + Application.instance.get_player_id());
+        if (pref == null) {
+            return Vienna.NAME;
+        }
+        return pref[0];
+    }
+
+    private _folders: Folder[] = [];
+    private static _instance: FolderManager;
+}
