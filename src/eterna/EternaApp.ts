b8ae2672fb9e7ad9a24a6646bbfc4fe602c97d24
Tim Conkling <tconkling@gmail.com>
2018-05-16 14:02:21 -0700

    Support dynamically loading our emscripten modules

--- src/eterna/EternaApp.ts
+++ src/eterna/EternaApp.ts
@@ -2,10 +2,7 @@ import {Application} from "pixi.js";
 import {AppMode} from "../flashbang/core/AppMode";
 import {FlashbangApp} from "../flashbang/core/FlashbangApp";
 import {Background} from "./Background";
-
-let vienna = require("./folding/engines/vienna");
-let vrna2 = require("./folding/engines/vrna2");
-let nupack = require("./folding/engines/nupack");
+import {Emscripten} from "./util/Emscripten";
 
 export class EternaApp extends FlashbangApp {
     protected createPixi (): Application {
@@ -18,34 +15,37 @@ export class EternaApp extends FlashbangApp {
 }
 
 class FoldingTester {
-    public constructor (name: string, engineFactory: any) {
+    public constructor (name: string, enginePath: string) {
         this._engineName = name;
         console.log(`[${this._engineName}] initializing...`);
 
-        engineFactory({noInitialRun: true}).then((engine: any) => {
-            this._engine = engine;
+        import('./folding/engines/vienna')
+            .then((module: any) => Emscripten.loadProgram(module))
+            .then((engine: any) => {
+                this._engine = engine;
 
-            if (engine.hasOwnProperty('callMain')) {
-                console.log(`[${this._engineName}] main()`);
-                engine.callMain();
-            }
+                if (engine.hasOwnProperty("callMain")) {
+                    engine.callMain();
+                }
 
-            console.log(`[${this._engineName}] initialized`);
+                console.log(`[${this._engineName}] initialized`);
 
-            // http://www.eternagame.org/web/puzzle/8284172/
-            const MINILOOP_SEQ = 'UAAAAAAG';
-            const MINILOOP_STRUCT = '((....))';
+                // http://www.eternagame.org/web/puzzle/8284172/
+                const MINILOOP_SEQ = 'UAAAAAAG';
+                const MINILOOP_STRUCT = '((....))';
 
-            // http://www.eternagame.org/web/puzzle/13833/
-            const SNOWFLAKE_SEQ = 'GUGGACAAGAUGAAACAUCAGUAACAAGCGCAAAGCGCGGGCAAAGCCCCCGGAAACCGGAAGUUACAGAACAAAGUUCAAGUUUACAAGUGGACAAGUUGAAACAACAGUUACAAGACGAAACGUCGGCCAAAGGCCCCAUAAAAUGGAAGUAACACUUGAAACAAGAAGUUUACAAGUUGACAAGUUCAAAGAACAGUUACAAGUGGAAACCACGCGCAAAGCGCCUCCAAAGGAGAAGUAACAGAAGAAACUUCAAGUUAGCAAGUGGUCAAGUACAAAGUACAGUAACAACAUCAAAGAUGGCGCAAAGCGCGAGCAAAGCUCAAGUUACAGAACAAAGUUCAAGAUUACAAGAGUGCAAGAAGAAACUUCAGAUAGAACUGCAAAGCAGCACCAAAGGUGGGGCAAAGCCCAACUAUCAGUUGAAACAACAAGUAUUCAAGAGGUCAAGAUCAAAGAUCAGUAACAAGUGCAAAGCACGGGCAAAGCCCGACCAAAGGUCAAGUUACAGUUCAAAGAACAAGAUUUC';
+                // http://www.eternagame.org/web/puzzle/13833/
+                const SNOWFLAKE_SEQ = 'GUGGACAAGAUGAAACAUCAGUAACAAGCGCAAAGCGCGGGCAAAGCCCCCGGAAACCGGAAGUUACAGAACAAAGUUCAAGUUUACAAGUGGACAAGUUGAAACAACAGUUACAAGACGAAACGUCGGCCAAAGGCCCCAUAAAAUGGAAGUAACACUUGAAACAAGAAGUUUACAAGUUGACAAGUUCAAAGAACAGUUACAAGUGGAAACCACGCGCAAAGCGCCUCCAAAGGAGAAGUAACAGAAGAAACUUCAAGUUAGCAAGUGGUCAAGUACAAAGUACAGUAACAACAUCAAAGAUGGCGCAAAGCGCGAGCAAAGCUCAAGUUACAGAACAAAGUUCAAGAUUACAAGAGUGCAAGAAGAAACUUCAGAUAGAACUGCAAAGCAGCACCAAAGGUGGGGCAAAGCCCAACUAUCAGUUGAAACAACAAGUAUUCAAGAGGUCAAGAUCAAAGAUCAGUAACAAGUGCAAAGCACGGGCAAAGCCCGACCAAAGGUCAAGUUACAGUUCAAAGAACAAGAUUUC';
 
-            const SNOWFLAKE_STRUCT = '((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))';
+                const SNOWFLAKE_STRUCT = '((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))';
 
-            this.FullFold('Miniloop', MINILOOP_SEQ, MINILOOP_STRUCT);
-            this.FullFold('Snowflake', SNOWFLAKE_SEQ, SNOWFLAKE_STRUCT);
-            this.FullFold('Snowflake', SNOWFLAKE_SEQ, SNOWFLAKE_STRUCT);
-            console.log('Done!');
-        });
+                this.FullFold('Miniloop', MINILOOP_SEQ, MINILOOP_STRUCT);
+                this.FullFold('Snowflake', SNOWFLAKE_SEQ, SNOWFLAKE_STRUCT);
+                this.FullFold('Snowflake', SNOWFLAKE_SEQ, SNOWFLAKE_STRUCT);
+                console.log('Done!');
+            }).catch((e) => {
+                console.error(e);
+            });
     }
 
     private FullFold (name: string, seq: string, struct: string) {
@@ -68,9 +68,10 @@ class TestMode extends AppMode {
     protected setup (): void {
         this.addObject(new Background(20, false), this.modeSprite);
         this._testers.push(
-            new FoldingTester("vienna", vienna),
-            new FoldingTester("vrna2", vrna2),
-            new FoldingTester("nupack", nupack));
+            new FoldingTester("vienna", "assets/engines/vienna.js"),
+            // new FoldingTester("vrna2", vrna2),
+            // new FoldingTester("nupack", nupack)
+        );
     }
 
     private _testers: FoldingTester[] = [];
