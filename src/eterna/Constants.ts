621a22ca2dabd1614e766d3b0483f24c6bba8099
Tim Conkling <tconkling@gmail.com>
2018-05-17 14:40:44 -0700

    Constants.ts

---
+++ src/eterna/Constants.ts
@@ -0,0 +1,6 @@
+﻿export class Constants {
+    public static EPSILON: number = 0.00001;
+
+    public static BOLTZMANN: number = 0.0019872041; // kcal/mol/K
+    public static KELVIN_0C: number = 273.15;
+}
