36b595c693ab3a14c901d8f3507a91e71570328c
Tim Conkling <tconkling@gmail.com>
2018-05-17 11:49:12 -0700

    Load our folding engines and register them with FolderManager

--- src/eterna/EternaApp.ts
+++ src/eterna/EternaApp.ts
@@ -2,19 +2,32 @@ import {Application} from "pixi.js";
 import {AppMode} from "../flashbang/core/AppMode";
 import {FlashbangApp} from "../flashbang/core/FlashbangApp";
 import {Background} from "./Background";
-import {EPars} from "./EPars";
-import {Eterna} from "./Eterna";
 import {Folder} from "./folding/Folder";
+import {FolderManager} from "./folding/FolderManager";
 import {Vienna} from "./folding/Vienna";
-import {AutosaveManager} from "./util/AutosaveManager";
+import * as log from 'loglevel';
 
 export class EternaApp extends FlashbangApp {
     protected createPixi (): Application {
         return new Application(1024, 768, {backgroundColor: 0x1099bb});
     }
 
+    /*override*/
     protected setup (): void {
         this._modeStack.pushMode(new TestMode());
+
+        this.loadFoldingEngines().catch((e) => {
+            log.error('Error loading folding engines', e);
+        });
+    }
+
+    private loadFoldingEngines (): Promise<void> {
+        log.info("Initializing folding engines...");
+        return Promise.all([Vienna.create()]).then((folders: Folder[]) => {
+            for (let folder of folders) {
+                FolderManager.instance.add_folder(folder);
+            }
+        });
     }
 }
 
@@ -22,19 +35,19 @@ class TestMode extends AppMode {
     protected setup (): void {
         this.addObject(new Background(20, false), this.modeSprite);
 
-        const SNOWFLAKE_SEQ = 'GUGGACAAGAUGAAACAUCAGUAACAAGCGCAAAGCGCGGGCAAAGCCCCCGGAAACCGGAAGUUACAGAACAAAGUUCAAGUUUACAAGUGGACAAGUUGAAACAACAGUUACAAGACGAAACGUCGGCCAAAGGCCCCAUAAAAUGGAAGUAACACUUGAAACAAGAAGUUUACAAGUUGACAAGUUCAAAGAACAGUUACAAGUGGAAACCACGCGCAAAGCGCCUCCAAAGGAGAAGUAACAGAAGAAACUUCAAGUUAGCAAGUGGUCAAGUACAAAGUACAGUAACAACAUCAAAGAUGGCGCAAAGCGCGAGCAAAGCUCAAGUUACAGAACAAAGUUCAAGAUUACAAGAGUGCAAGAAGAAACUUCAGAUAGAACUGCAAAGCAGCACCAAAGGUGGGGCAAAGCCCAACUAUCAGUUGAAACAACAAGUAUUCAAGAGGUCAAGAUCAAAGAUCAGUAACAAGUGCAAAGCACGGGCAAAGCCCGACCAAAGGUCAAGUUACAGUUCAAAGAACAAGAUUUC';
-
-        const SNOWFLAKE_STRUCT = '((((((..(((.....))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))';
-
-        Vienna.Create().then((vienna: Folder) => {
-            let result = vienna.fold_sequence(EPars.string_to_sequence_array(SNOWFLAKE_SEQ), null, SNOWFLAKE_STRUCT);
-            console.log(result);
-
-            AutosaveManager.saveObjects([vienna.get_folder_name()], "folder-" + Eterna.player_id);
-            let pref: any[] = AutosaveManager.loadObjects("folder-" + Eterna.player_id);
-            let name: string = pref === null ? Vienna.NAME : pref[0];
-            console.log(name);
-            AutosaveManager.clear();
-        });
+        // const SNOWFLAKE_SEQ = 'GUGGACAAGAUGAAACAUCAGUAACAAGCGCAAAGCGCGGGCAAAGCCCCCGGAAACCGGAAGUUACAGAACAAAGUUCAAGUUUACAAGUGGACAAGUUGAAACAACAGUUACAAGACGAAACGUCGGCCAAAGGCCCCAUAAAAUGGAAGUAACACUUGAAACAAGAAGUUUACAAGUUGACAAGUUCAAAGAACAGUUACAAGUGGAAACCACGCGCAAAGCGCCUCCAAAGGAGAAGUAACAGAAGAAACUUCAAGUUAGCAAGUGGUCAAGUACAAAGUACAGUAACAACAUCAAAGAUGGCGCAAAGCGCGAGCAAAGCUCAAGUUACAGAACAAAGUUCAAGAUUACAAGAGUGCAAGAAGAAACUUCAGAUAGAACUGCAAAGCAGCACCAAAGGUGGGGCAAAGCCCAACUAUCAGUUGAAACAACAAGUAUUCAAGAGGUCAAGAUCAAAGAUCAGUAACAAGUGCAAAGCACGGGCAAAGCCCGACCAAAGGUCAAGUUACAGUUCAAAGAACAAGAUUUC';
+        //
+        // const SNOWFLAKE_STRUCT = '((((((..(((.....))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))';
+        //
+        // Vienna.create().then((vienna: Folder) => {
+        //     let result = vienna.fold_sequence(EPars.string_to_sequence_array(SNOWFLAKE_SEQ), null, SNOWFLAKE_STRUCT);
+        //     console.log(result);
+        //
+        //     AutosaveManager.saveObjects([vienna.get_folder_name()], "folder-" + Eterna.player_id);
+        //     let pref: any[] = AutosaveManager.loadObjects("folder-" + Eterna.player_id);
+        //     let name: string = pref === null ? Vienna.NAME : pref[0];
+        //     console.log(name);
+        //     AutosaveManager.clear();
+        // });
     }
 }
--- src/eterna/index.ts
+++ src/eterna/index.ts
@@ -1,4 +1,8 @@
+import * as log from "loglevel";
 import {EternaApp} from "./EternaApp";
+
+const isProduction = process.env.NODE_ENV === 'production';
+log.setLevel(isProduction ? 'warn' : 'trace');
 
 let app: EternaApp = new EternaApp();
 app.run();
