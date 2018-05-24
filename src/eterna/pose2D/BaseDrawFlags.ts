48a1b0fe27d9dde7fea10b6e73fb1a9f1b7d2456
Tim Conkling <tconkling@gmail.com>
2018-05-24 14:18:31 -0700

    more work on Pose2D

--- src/eterna/pose2D/BaseDrawFlags.ts
+++ src/eterna/pose2D/BaseDrawFlags.ts
@@ -1,39 +1,31 @@
 /** View state flags for Base.as rendering */
 export class BaseDrawFlags {
-    /** If set, the base is not currently animating */
-    public static STATIC: number = 1 << 0;
-
     /** If set, the base will have a lock icon on it */
-    public static LOCKED: number = 1 << 1;
+    public static LOCKED: number = 1 << 0;
 
     /** If set, the base will have its letter (A, C, G, or U) drawn on top of it at higher zoom levels */
-    public static LETTER_MODE: number = 1 << 2;
+    public static LETTER_MODE: number = 1 << 1;
 
     /** If set, fancy animations and effects are disabled */
-    public static LOW_PERFORM: number = 1 << 3;
+    public static LOW_PERFORM: number = 1 << 2;
 
     /**
      * Set if the base is part of a "unique marker sequence".
      * The base will be drawn with a white stippled outline around it.
      */
-    public static USE_BARCODE: number = 1 << 4;
+    public static USE_BARCODE: number = 1 << 3;
 
     /**
      * Set if the location this base folds to doesn't matter.
      * The base will be drawn with a purple hilite (instead of white)
      */
-    public static IS_DONTCARE: number = 1 << 5;
+    public static IS_DONTCARE: number = 1 << 4;
 
     /** Fluent builder functions */
     public static builder(initialFlags: number = 0): BaseDrawFlags {
         let out: BaseDrawFlags = new BaseDrawFlags();
         out._flags = initialFlags;
         return out;
-    }
-
-    public setStatic(val: boolean = true): BaseDrawFlags {
-        this._flags = (val ? this._flags | BaseDrawFlags.STATIC : this._flags & ~BaseDrawFlags.STATIC);
-        return this;
     }
 
     public locked(val: boolean = true): BaseDrawFlags {
---
+++ src/eterna/pose2D/PoseUtil.ts
@@ -0,0 +1,133 @@
+import {EPars} from "../EPars";
+
+/** Utility functions for pose data */
+export class PoseUtil {
+    public static get_pair_strength(s1: number, s2: number): number {
+        if (PoseUtil.isPair(s1, s2, EPars.RNABASE_ADENINE, EPars.RNABASE_URACIL)) {
+            return 2;
+        } else if (PoseUtil.isPair(s1, s2, EPars.RNABASE_GUANINE, EPars.RNABASE_URACIL)) {
+            return 1;
+        } else if (PoseUtil.isPair(s1, s2, EPars.RNABASE_GUANINE, EPars.RNABASE_CYTOSINE)) {
+            return 3;
+        } else {
+            return -1;
+        }
+    }
+
+    public static add_base_with_index(index: number, pairs: number[]): any[] {
+        let mutated_pairs: number[];
+        mutated_pairs = pairs.slice(0, index);
+        mutated_pairs.push(-1);
+        mutated_pairs = mutated_pairs.concat(pairs.slice(index, pairs.length));
+
+        for (let ii: number = 0; ii < mutated_pairs.length; ii++) {
+            if (mutated_pairs[ii] >= index) {
+                mutated_pairs[ii]++;
+            }
+        }
+        let parenthesis: string = EPars.pairs_array_to_parenthesis(mutated_pairs);
+        return [parenthesis, 0, mutated_pairs];
+    }
+
+    public static add_pair_with_index(index: number, pairs: number[]): any[] {
+        let mutated_pairs: number[];
+        let parenthesis: string;
+        let ii: number;
+
+        //if index is paired
+        //add another pair before index
+        let pindex: number = pairs[index];
+        if (pindex >= 0) {
+            if (index > pindex) {
+                let tmp: number = index;
+                index = pindex;
+                pindex = tmp;
+            }
+            mutated_pairs = pairs.slice(0, index);
+            mutated_pairs.push(-1);
+            mutated_pairs = mutated_pairs.concat(pairs.slice(index, pindex + 1));
+            mutated_pairs.push(-1);
+            mutated_pairs = mutated_pairs.concat(pairs.slice(pindex + 1, pairs.length));
+
+            for (ii = 0; ii < mutated_pairs.length; ii++) {
+                if (mutated_pairs[ii] > pindex) {
+                    mutated_pairs[ii] += 2;
+                } else if (mutated_pairs[ii] >= index) {
+                    mutated_pairs[ii] += 1;
+                }
+            }
+            mutated_pairs[index] = pindex + 2;
+            mutated_pairs[pindex + 2] = index;
+
+            parenthesis = EPars.pairs_array_to_parenthesis(mutated_pairs);
+            return [parenthesis, 1];
+
+        }
+        //add a cycle of length 3
+        else {
+            mutated_pairs = pairs.slice(0, index);
+            for (ii = 0; ii < 5; ii++) {
+                mutated_pairs.push(-1);
+            }
+            mutated_pairs = mutated_pairs.concat(pairs.slice(index, pairs.length));
+            for (ii = 0; ii < mutated_pairs.length; ii++) {
+                if (mutated_pairs[ii] >= index) {
+                    mutated_pairs[ii] += 5;
+                }
+            }
+            mutated_pairs[index] = index + 4;
+            mutated_pairs[index + 4] = index;
+
+            parenthesis = EPars.pairs_array_to_parenthesis(mutated_pairs);
+            return [parenthesis, 2];
+        }
+    }
+
+    public static delete_nopair_with_index(index: number, pairs: number[]): any[] {
+        let mutated_pairs: number[];
+        let parenthesis: string;
+        mutated_pairs = pairs.slice(0, index);
+        mutated_pairs = mutated_pairs.concat(pairs.slice(index + 1, pairs.length));
+        for (let ii: number = 0; ii < mutated_pairs.length; ii++) {
+            if (mutated_pairs[ii] >= index) {
+                mutated_pairs[ii] -= 1;
+            }
+        }
+
+        parenthesis = EPars.pairs_array_to_parenthesis(mutated_pairs);
+        return [parenthesis, 4, mutated_pairs];
+    }
+
+    public static delete_pair_with_index(index: number, pairs: number[]): any[] {
+        let pindex: number = pairs[index];
+        if (pindex < 0) {
+            throw new Error("base doesn't have pair");
+        }
+
+        let mutated_pairs: number[];
+        let parenthesis: string;
+
+        if (index > pindex) {
+            let tmp: number = index;
+            index = pindex;
+            pindex = tmp;
+        }
+        mutated_pairs = pairs.slice(0, index);
+        mutated_pairs = mutated_pairs.concat(pairs.slice(index + 1, pindex));
+        mutated_pairs = mutated_pairs.concat(pairs.slice(pindex + 1, pairs.length));
+        for (let ii: number = 0; ii < mutated_pairs.length; ii++) {
+            if (mutated_pairs[ii] > pindex) {
+                mutated_pairs[ii] -= 2;
+            } else if (mutated_pairs[ii] >= index) {
+                mutated_pairs[ii] -= 1;
+            }
+        }
+
+        parenthesis = EPars.pairs_array_to_parenthesis(mutated_pairs);
+        return [parenthesis, 3];
+    }
+
+    private static isPair(s1: number, s2: number, type1: number, type2: number): boolean {
+        return (s1 == type1 && s2 == type2) || (s1 == type2 && s2 == type1);
+    }
+}
