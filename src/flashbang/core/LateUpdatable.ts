6196498a18478ef7b7b82d7208e8b2c91edf7536
Tim Conkling <tconkling@gmail.com>
2018-05-23 18:18:54 -0700

    Flashbang: LateUpdatable

---
+++ src/flashbang/core/LateUpdatable.ts
@@ -0,0 +1,8 @@
+export interface LateUpdatable {
+    /**
+     * Update this object. dt is the number of seconds that have elapsed since the last update.
+     * (lateUpdate() is called after all update() functions have completed. They're a good place
+     * to handle rendering logic.)s
+     */
+    lateUpdate(dt: number): void;
+}
