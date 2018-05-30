bfc727abf749bd8884ffb14b88b021f079dffbc6
Tim Conkling <tconkling@gmail.com>
2018-05-30 16:10:47 -0700

    flashbang: SceneObject base class
    SceneObjects are GameObjects that wrap DisplayObjects and can emit
    pointer-related event signals

---
+++ src/flashbang/objects/SceneObject.ts
@@ -0,0 +1,53 @@
+import {DisplayObject} from "pixi.js";
+import {SignalView} from "../../signals/SignalView";
+import {GameObject} from "../core/GameObject";
+import {PointerTarget} from "../input/PointerTarget";
+import {DisplayObjectPointerTarget} from "../input/DisplayObjectPointerTarget";
+
+type InteractionEvent = PIXI.interaction.InteractionEvent;
+
+/** A convenience class that manages a displayObject directly. */
+export class SceneObject extends GameObject implements PointerTarget {
+    constructor(displayObject: DisplayObject) {
+        super();
+        this._displayObject = displayObject;
+    }
+
+    public /*final*/ get display(): DisplayObject {
+        return this._displayObject;
+    }
+
+    public get target(): DisplayObject {
+        return this._displayObject;
+    }
+
+    public get pointerOver(): SignalView<InteractionEvent> {
+        return this.getPointerTarget().pointerOver;
+    }
+
+    public get pointerOut(): SignalView<InteractionEvent> {
+        return this.getPointerTarget().pointerOut;
+    }
+
+    public get pointerDown(): SignalView<InteractionEvent> {
+        return this.getPointerTarget().pointerDown;
+    }
+
+    public get pointerMove(): SignalView<InteractionEvent> {
+        return this.getPointerTarget().pointerMove;
+    }
+
+    public get pointerUp(): SignalView<InteractionEvent> {
+        return this.getPointerTarget().pointerMove;
+    }
+
+    protected getPointerTarget(): PointerTarget {
+        if (this._pointerTarget == null) {
+            this._pointerTarget = new DisplayObjectPointerTarget(this._displayObject);
+        }
+        return this._pointerTarget;
+    }
+
+    protected _displayObject: DisplayObject;
+    protected _pointerTarget: PointerTarget; // lazily instantiated
+}
---
+++ src/flashbang/util/EventSignal.ts
@@ -0,0 +1,64 @@
+import {AbstractSignal} from "../../signals/AbstractSignal";
+import {FilteredSignal} from "../../signals/FilteredSignal";
+import {MappedSignal} from "../../signals/MappedSignal";
+import {SignalView} from "../../signals/SignalView";
+
+type InteractionEvent = PIXI.interaction.InteractionEvent;
+type EventEmitter = PIXI.utils.EventEmitter;
+
+/** Redispatches a pixi InteractionEvent as a Signal */
+export class EventSignal extends AbstractSignal<InteractionEvent> implements SignalView<InteractionEvent> {
+    public constructor(target: EventEmitter, eventType: string | symbol) {
+        super();
+        this._target = target;
+        this._eventType = eventType;
+    }
+
+    public emit(e: InteractionEvent) {
+        this.notifyEmit(e);
+    }
+
+    public map<U>(func: (value: InteractionEvent) => U): SignalView<U> {
+        return MappedSignal.create(this, func);
+    }
+
+    public filter(pred: (value: InteractionEvent) => boolean): SignalView<InteractionEvent> {
+        return new FilteredSignal(this, pred);
+    }
+
+    /*override*/
+    protected connectionAdded(): void {
+        super.connectionAdded();
+        if (!this._connected) {
+            this.connectToSource();
+        }
+    }
+
+    /*override*/
+    protected connectionRemoved(): void {
+        super.connectionRemoved();
+        if (!this.hasConnections && this._connected) {
+            this.disconnectFromSource();
+        }
+    }
+
+    protected connectToSource(): void {
+        if (!this._connected) {
+            this._connected = true;
+            this._target.addListener(this._eventType, this._emitEvent);
+        }
+    }
+
+    protected disconnectFromSource(): void {
+        if (this._connected) {
+            this._connected = false;
+            this._target.removeListener(this._eventType, this._emitEvent);
+        }
+    }
+
+    protected _target: EventEmitter;
+    protected _eventType: string | symbol;
+    protected _connected: boolean;
+
+    private readonly _emitEvent = (e: InteractionEvent) => this.notifyEmit(e);
+}
