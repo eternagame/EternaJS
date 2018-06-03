87ff8a2a0800a03b2f20e13e9ef7a8ab97a816ac
Tim Conkling <tconkling@gmail.com>
2018-06-03 14:38:44 -0700

    unused

--- src/eterna/Eterna.ts
+++ src/eterna/Eterna.ts
@@ -3,9 +3,6 @@ import {GameClient} from "./net/GameClient";
 
 /** Constants and managers */
 export class Eterna {
-    public static readonly GET_URI: String = "/get/";
-    public static readonly POST_URI: String = "/post/";
-
     public static player_id: number = 1;
     public static is_dev_mode: boolean = true;
 
