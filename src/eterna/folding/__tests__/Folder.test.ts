ff59cf03c40a55e1b98249e0b65429f742a736a0
Tim Conkling <tconkling@gmail.com>
2018-06-15 17:27:16 -0700

    score_structures folding tests

--- src/eterna/folding/__tests__/Folder.test.ts
+++ src/eterna/folding/__tests__/Folder.test.ts
@@ -8,7 +8,11 @@ const SNOWFLAKE_SEQ: string = 'GUGGACAAGAUGAAACAUCAGUAACAAGCGCAAAGCGCGGGCAAAGCCC
 const SNOWFLAKE_STRUCT: string = '((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))..((((((..((((...)))).(((((..((((...))))((((...))))((((...))))..))))).((((...))))..))))))';
 
 const BASIC_SEQ: string = "AAAAAAAAAAAAAA";
-const BASIC_RESULT: number[] = [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1];
+const BASIC_RESULT: number[] = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
+
+const ZIPPERS_SEQ: string = "AAAAAGGGGAAAAAAAAACCCCAGCGGAAAAAACUGCAAA";
+const ZIPPERS_BEST_PAIRS: string = ".....((((.........)))).((((......))))...";
+const ZIPPERS_TEMP = 37;
 
 function FoldSequence(folder: Folder, seq: string, struct: string): any[] {
     return folder.fold_sequence(EPars.string_to_sequence_array(seq), null, struct);
@@ -45,5 +49,39 @@ for (let folderType of [Vienna, Vienna2, NuPACK]) {
                 ];
             }))
             .resolves.toEqual([BASIC_RESULT, BASIC_RESULT]);
+    });
+
+    test(`${folderType.NAME}:score_structures`, () => {
+        // The engines output different results
+
+        const TOTAL_FE: Map<string, number> = new Map([
+            [Vienna.NAME, -1080],
+            [Vienna2.NAME, -1019.999],
+            [NuPACK.NAME, -1110]
+        ]);
+
+        const NNFE: Map<string, number[]> = new Map([
+            [Vienna.NAME, [-1,-360,5,-330,6,-330,7,-330,8,530,23,-340,24,-140,25,-210,26,430]],
+            [Vienna2.NAME, [-1,-300,5,-330,6,-330,7,-330,8,530,23,-340,24,-140,25,-210,26,430]],
+            [NuPACK.NAME, [-1,-360,26,360,25,-190,24,-120,23,-340,8,410,7,-290,6,-290,5,-290]],
+        ]);
+
+        let expectedTotalFe = TOTAL_FE.get(folderType.NAME);
+        let expectedNNFE = NNFE.get(folderType.NAME);
+
+        expect.assertions(3);
+        return expect(CreateFolder(folderType)
+            .then((folder) => {
+                let outNNFE: number[] = [];
+                let totalFe = folder.score_structures(
+                    EPars.string_to_sequence_array(ZIPPERS_SEQ),
+                    EPars.parenthesis_to_pair_array(ZIPPERS_BEST_PAIRS),
+                    ZIPPERS_TEMP,
+                    outNNFE);
+
+                expect(totalFe).toBeCloseTo(expectedTotalFe);
+                expect(outNNFE).toEqual(expectedNNFE);
+            }))
+            .resolves.toBeUndefined(); // (we're returning a promise)
     });
 }
