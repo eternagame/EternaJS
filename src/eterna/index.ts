f54a329c5543008d68ff384a88d845c46b69eabc
Tim Conkling <tconkling@gmail.com>
2018-06-18 12:19:19 -0700

    Show info traces on production

--- src/eterna/index.ts
+++ src/eterna/index.ts
@@ -2,7 +2,7 @@ import * as log from "loglevel";
 import {EternaApp} from "./EternaApp";
 
 const isProduction = process.env.NODE_ENV === 'production';
-log.setLevel(isProduction ? 'warn' : 'trace');
+log.setLevel(isProduction ? 'info' : 'trace');
 
 let app: EternaApp = new EternaApp();
 app.run();
