704155bbb316893832622312f21157f92904172a
Tim Conkling <tconkling@gmail.com>
2018-06-18 17:26:53 -0700

    Settings system, using local storage

---
+++ src/flashbang/settings/Setting.ts
@@ -0,0 +1,50 @@
+import {AbstractValue} from "../../signals/AbstractValue";
+import {MappedValue} from "../../signals/MappedValue";
+import {ValueView} from "../../signals/ValueView";
+
+export class Setting<T> extends AbstractValue<T> {
+    public constructor(store: StoreJsAPI, name: string, defaultVal: T) {
+        super();
+        this._store = store;
+        this._name = name;
+        this._defaultVal = defaultVal;
+    }
+
+    /** Returns true if the setting has been created (even if its value is null) */
+    public get exists(): boolean {
+        return this._store.get(this._name) !== undefined;
+    }
+
+    /**
+     * Updates this instance with the supplied value. Registered listeners are notified only if the
+     * value differs from the current value.
+     * @return the previous value contained by this instance.
+     */
+    public set value(value: T) {
+        this.updateAndNotifyIf(value);
+    }
+
+    /*override*/
+    public get value(): T {
+        return (this.exists ? this._store.get(this._name) : this._defaultVal);
+    }
+
+    public remove(): void {
+        this._store.remove(this._name);
+    }
+
+    public map<U>(func: (value: T) => U): ValueView<U> {
+        return MappedValue.create(this, func);
+    }
+
+    /*override*/
+    protected updateLocal(value: T): T {
+        let oldValue: T = this.value;
+        this._store.set(this._name, value);
+        return oldValue;
+    }
+
+    private readonly _store: StoreJsAPI;
+    private readonly _name: string;
+    private readonly _defaultVal: T;
+}
