baeb6220b0dcc6806bdeefe735c2f47a298ed362
Tim Conkling <tconkling@gmail.com>
2018-06-20 13:53:04 -0700

    NucleotidePalette: hotkeys

--- src/eterna/ui/NucleotidePalette.ts
+++ src/eterna/ui/NucleotidePalette.ts
@@ -1,5 +1,7 @@
 import * as log from "loglevel";
 import {Point, Rectangle, Sprite, Text, Texture} from "pixi.js";
+import {KeyboardEventType} from "../../flashbang/input/KeyboardEventType";
+import {KeyboardListener} from "../../flashbang/input/KeyboardInput";
 import {KeyCode} from "../../flashbang/input/KeyCode";
 import {ContainerObject} from "../../flashbang/objects/ContainerObject";
 import {Signal} from "../../signals/Signal";
@@ -30,7 +32,7 @@ export function GetPaletteTargetBaseType(type: PaletteTargetType): number {
  * Nucleotide palette class. Handles the AUCG nucleotides options as well as the pairs.
  * Has the option to turn into a 'no pair' mode.
  */
-export class NucleotidePalette extends ContainerObject {
+export class NucleotidePalette extends ContainerObject implements KeyboardListener {
     /** Emitted when a palette target is clicked */
     public readonly targetClicked: Signal<PaletteTargetType> = new Signal();
 
@@ -96,9 +98,14 @@ export class NucleotidePalette extends ContainerObject {
 
         this._enabled = true;
         this._last_tooltip = null;
+    }
+
+    protected added(): void {
+        super.added();
 
         this.regs.add(this.pointerDown.connect((e) => this.on_click(e)));
         this.regs.add(this.pointerMove.connect((e) => this.on_move_mouse(e)));
+        this.regs.add(this.mode.keyboardInput.pushListener(this));
     }
 
     public set_override_default(): void {
@@ -146,33 +153,39 @@ export class NucleotidePalette extends ContainerObject {
         return this._palette_display.width;
     }
 
-    /*override*/
-    // public on_key_down(key: number, ctrl: boolean, shift: boolean): boolean {
-    //     if (key == KeyCode.KEY_1) {
-    //         this.click_a();
-    //         return true;
-    //     } else if (key == KeyCode.KEY_2) {
-    //         this.click_u();
-    //         return true;
-    //     } else if (key == KeyCode.KEY_3) {
-    //         this.click_g();
-    //         return true;
-    //     } else if (key == KeyCode.KEY_4) {
-    //         this.click_c();
-    //         return true;
-    //     } else if (key == KeyCode.KEY_Q) {
-    //         this.click_au();
-    //         return true;
-    //     } else if (key == KeyCode.KEY_W) {
-    //         this.click_ug();
-    //         return true;
-    //     } else if (key == KeyCode.KEY_E) {
-    //         this.click_gc();
-    //         return true;
-    //     }
-    //
-    //     return super.on_key_down(key, ctrl, shift);
-    // }
+    public onKeyboardEvent(e: KeyboardEvent): boolean {
+        if (!this._enabled) {
+            return false;
+        }
+
+        if (e.type == KeyboardEventType.KEY_DOWN) {
+            switch(e.code) {
+            case KeyCode.Digit1:
+                this.clickTarget(PaletteTargetType.A);
+                return true;
+            case KeyCode.Digit2:
+                this.clickTarget(PaletteTargetType.U);
+                return true;
+            case KeyCode.Digit3:
+                this.clickTarget(PaletteTargetType.G);
+                return true;
+            case KeyCode.Digit4:
+                this.clickTarget(PaletteTargetType.C);
+                return true;
+            case KeyCode.KeyQ:
+                this.clickTarget(PaletteTargetType.AU);
+                return true;
+            case KeyCode.KeyW:
+                this.clickTarget(PaletteTargetType.UG);
+                return true;
+            case KeyCode.KeyE:
+                this.clickTarget(PaletteTargetType.GC);
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public clickTarget(type: PaletteTargetType): void {
         let target: PaletteTarget = this._targets[type];
