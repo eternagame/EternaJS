be8d52c42da257e544d379c091d30661eef77bdb
Tim Conkling <tconkling@gmail.com>
2018-06-21 17:23:44 -0700

    UndoBlock creates Plots on demand, rather than storing them

--- src/eterna/ui/SpecBox.ts
+++ src/eterna/ui/SpecBox.ts
@@ -2,10 +2,10 @@ import * as log from "loglevel";
 import {Point, Sprite, Text} from "pixi.js";
 import {DisplayObjectPointerTarget} from "../../flashbang/input/DisplayObjectPointerTarget";
 import {KeyCode} from "../../flashbang/input/KeyCode";
-import {EPars} from "../EPars";
+import {ContainerObject} from "../../flashbang/objects/ContainerObject";
 import {EternaURL} from "../net/EternaURL";
 import {Plot} from "../Plot";
-import {UndoBlock, UndoBlockParam} from "../UndoBlock";
+import {UndoBlock} from "../UndoBlock";
 import {BitmapManager} from "../util/BitmapManager";
 import {Fonts} from "../util/Fonts";
 import {GameButton} from "./GameButton";
@@ -14,12 +14,15 @@ import {TextBalloon} from "./TextBalloon";
 
 type InteractionEvent = PIXI.interaction.InteractionEvent;
 
-export class SpecBox extends GamePanel {
+export class SpecBox extends ContainerObject {
     constructor(docked: boolean = false) {
         super();
+
+        this._panel = new GamePanel();
         if (!docked) {
-            this.setup(0, 1.0, 0x152843, 0.27, 0xC0DCE7);
+            this._panel.setup(0, 1.0, 0x152843, 0.27, 0xC0DCE7);
         }
+        this.addObject(this._panel, this.container);
 
         this._docked = docked;
         this._dotplotOriginX = 0;
@@ -69,7 +72,6 @@ export class SpecBox extends GamePanel {
             .tooltip("Zoom In")
             .hotkey(KeyCode.KeyI);
         this._zoom_in.clicked.connect(() => this.dotPlotZoomIn());
-        this._zoom_in.display.position = new Point(40, this.get_panel_height() - 125);
         this.addObject(this._zoom_in, this.container);
 
         this._zoom_out = new GameButton()
@@ -77,7 +79,6 @@ export class SpecBox extends GamePanel {
             .tooltip("Zoom out")
             .hotkey(KeyCode.KeyO);
         this._zoom_out.clicked.connect(() => this.dotPlotZoomOut());
-        this._zoom_out.display.position = new Point(70, this.get_panel_height() - 130);
         this.addObject(this._zoom_out, this.container);
 
         if (docked) {
@@ -89,20 +90,43 @@ export class SpecBox extends GamePanel {
         }
     }
 
+    protected added(): void {
+        super.added();
+        this.updateLayout();
+    }
+
+    public get width(): number {
+        return this._width;
+    }
+
+    public get height(): number {
+        return this._height;
+    }
+
+    public set_size(width: number, height: number): void {
+        if (this._width != width || this._height != height) {
+            this._width = width;
+            this._height = height;
+            if (this.isLiveObject) {
+                this.updateLayout();
+            }
+        }
+    }
+
     public set_spec(datablock: UndoBlock): void {
         let temperature: number = 37;
 
         this._datasize = datablock.get_sequence().length;
 
-        this._dotplot = datablock.get_dotplot();
-        this._meltplot = datablock.get_meltplot();
+        this._dotplot = datablock.create_dotplot();
+        this._meltplot = datablock.create_meltplot();
 
         let statstring: string = "";
-        statstring += "<B>" + EPars.get_colored_letter("A") + "-" + EPars.get_colored_letter("U") + " pairs : </B>" + datablock.get_param(UndoBlockParam.AU, temperature) + "   ";
-        statstring += "<B>" + EPars.get_colored_letter("G") + "-" + EPars.get_colored_letter("C") + " pairs : </B>" + datablock.get_param(UndoBlockParam.GC, temperature) + "   ";
-        statstring += "<B>" + EPars.get_colored_letter("G") + "-" + EPars.get_colored_letter("U") + " pairs : </B>" + datablock.get_param(UndoBlockParam.GU, temperature) + "\n";
-        statstring += "<B>Melting point : </B>" + datablock.get_param(UndoBlockParam.MELTING_POINT, temperature) + "°C\n";
-        statstring += "<B>Free energy : </B>" + (datablock.get_param(UndoBlockParam.FE, temperature) / 100) + "kcal\n";
+        // statstring += "<B>" + EPars.get_colored_letter("A") + "-" + EPars.get_colored_letter("U") + " pairs : </B>" + datablock.get_param(UndoBlockParam.AU, temperature) + "   ";
+        // statstring += "<B>" + EPars.get_colored_letter("G") + "-" + EPars.get_colored_letter("C") + " pairs : </B>" + datablock.get_param(UndoBlockParam.GC, temperature) + "   ";
+        // statstring += "<B>" + EPars.get_colored_letter("G") + "-" + EPars.get_colored_letter("U") + " pairs : </B>" + datablock.get_param(UndoBlockParam.GU, temperature) + "\n";
+        // statstring += "<B>Melting point : </B>" + datablock.get_param(UndoBlockParam.MELTING_POINT, temperature) + "°C\n";
+        // statstring += "<B>Free energy : </B>" + (datablock.get_param(UndoBlockParam.FE, temperature) / 100) + "kcal\n";
         this._stattext.text = statstring;
 
         if (this._hvec != null) {
@@ -134,7 +158,7 @@ export class SpecBox extends GamePanel {
         this._dotplotOriginY = 0;
         this._dotplotScaleLevel = 1;
 
-        this.updateSpecBoxView();
+        this.updateLayout();
     }
 
     public dotPlotZoomIn(): void {
@@ -159,11 +183,11 @@ export class SpecBox extends GamePanel {
         let plot_h: number;
 
         if (this._docked) {
-            plot_w = this.get_panel_width() - 55;
-            plot_h = (this.get_panel_height() - 51) / 2.0;
+            plot_w = this._width - 55;
+            plot_h = (this._height - 51) / 2.0;
         } else {
-            plot_w = (this.get_panel_width() - 100) / 2.0;
-            plot_h = this.get_panel_height() - 200;
+            plot_w = (this._width - 100) / 2.0;
+            plot_h = this._height - 200;
         }
 
         return Math.min(plot_w, plot_h);
@@ -190,13 +214,12 @@ export class SpecBox extends GamePanel {
         }
     }
 
-    private updateSpecBoxView(): void {
-        let panelWidth = this.get_panel_width();
-        let panelHeight = this.get_panel_height();
+    private updateLayout(): void {
+        this._panel.set_size(this._width, this._height);
 
         if (this._docked) {
             this._dotplot_canvas.position = new Point(20, 15);
-            this._meltplot_canvas.position = new Point(20, (panelHeight * 0.5) + 8);
+            this._meltplot_canvas.position = new Point(20, (this._height * 0.5) + 8);
 
             this._stattext.visible = false;
             this._helptext.visible = false;
@@ -207,29 +230,31 @@ export class SpecBox extends GamePanel {
             this._zoom_out.display.visible = false;
 
         } else {
-            this.set_panel_title("RNA Spec");
+            this._panel.set_panel_title("RNA Spec");
 
             this._v0.position = new Point(40 - this._v0.width - 3, 70);
 
-            this._vn_melt.position.x = (panelWidth * 0.5) + 25 - this._vn_melt.width - 3;
+            this._vn_melt.position.x = (this._width * 0.5) + 25 - this._vn_melt.width - 3;
             this._vn_melt.position.y = 70;
 
             this._dotplot_canvas.position = new Point(40, 70);
-            this._meltplot_canvas.position = new Point((panelWidth * 0.5) + 20, 70);
+            this._meltplot_canvas.position = new Point((this._width * 0.5) + 20, 70);
 
             this._stattext.visible = true;
-            this._stattext.position = new Point(20, panelHeight - 100);
+            this._stattext.position = new Point(20, this._height - 100);
             this._stattext.position = new Point(200, 200);
 
             this._helptext.visible = true;
-            this._helptext.position = new Point(20, panelHeight - 35);
+            this._helptext.position = new Point(20, this._height - 35);
             this._dotplottext.visible = true;
             this._dotplottext.position = new Point(30, 40);
             this._meltplottext.visible = true;
-            this._meltplottext.position = new Point((panelWidth * 0.5) + 10, 50);
+            this._meltplottext.position = new Point((this._width * 0.5) + 10, 50);
 
             this._zoom_in.display.visible = true;
             this._zoom_out.display.visible = true;
+            this._zoom_in.display.position = new Point(40, this._height - 125);
+            this._zoom_out.display.position = new Point(70, this._height - 130);
         }
 
         let plot_size: number = this.getPlotSize();
@@ -243,23 +268,23 @@ export class SpecBox extends GamePanel {
             this._meltplot_canvas.addChild(this._meltplot);
 
             if (this._docked) {
-                this._h0_melt.position = new Point(15, (panelHeight * 0.5) + plot_size + 10);
+                this._h0_melt.position = new Point(15, (this._height * 0.5) + plot_size + 10);
                 this._hn_melt.position = new Point(
                     25 + plot_size - this._hn_melt.width,
-                    (panelHeight * 0.5) + plot_size + 10);
+                    (this._height * 0.5) + plot_size + 10);
                 this._v0_melt.position = new Point(
                     25 - this._v0_melt.width - 3,
-                    (panelHeight * 0.5) + plot_size - 10);
+                    (this._height * 0.5) + plot_size - 10);
                 this._vn_melt.position = new Point(
                     25 - this._vn_melt.width - 3,
-                    (panelHeight * 0.5) + 5);
+                    (this._height * 0.5) + 5);
             } else {
-                this._h0_melt.position = new Point((panelWidth * 0.5) + 15, plot_size + 75);
+                this._h0_melt.position = new Point((this._width * 0.5) + 15, plot_size + 75);
                 this._hn_melt.position = new Point(
-                    (panelWidth * 0.5) + 25 + plot_size - this._hn_melt.width,
+                    (this._width * 0.5) + 25 + plot_size - this._hn_melt.width,
                     plot_size + 75);
                 this._v0_melt.position = new Point(
-                    (panelWidth * 0.5) + 25 - this._v0_melt.width - 3,
+                    (this._width * 0.5) + 25 - this._v0_melt.width - 3,
                     55 + plot_size);
             }
         }
@@ -402,28 +427,33 @@ export class SpecBox extends GamePanel {
         }
     }
 
+
+    private readonly _docked: boolean;
+
+    private readonly _panel: GamePanel;
+    private readonly _zoom_in: GameButton;
+    private readonly _zoom_out: GameButton;
+    private readonly _dotplot_canvas: Sprite;
+    private readonly _meltplot_canvas: Sprite;
+    private readonly _h0: Text;
+    private readonly _h0_melt: Text;
+    private readonly _hn_melt: Text;
+    private readonly _v0: Text;
+    private readonly _v0_melt: Text;
+    private readonly _vn_melt: Text;
+    private readonly _stattext: Text;
+    private readonly _helptext: Text;
+    private readonly _dotplottext: Text;
+    private readonly _meltplottext: Text;
+
+    private _coordBalloon: TextBalloon;
     private _dotplot: Plot;
-    private _dotplot_canvas: Sprite;
     private _meltplot: Plot;
-    private _meltplot_canvas: Sprite;
-    private _h0: Text;
-    private _h0_melt: Text;
-    private _hn_melt: Text;
-    private _v0: Text;
-    private _v0_melt: Text;
-    private _vn_melt: Text;
     private _datasize: number = 0;
     private _hvec: Text[];
     private _vvec: Text[];
-    private _stattext: Text;
-    private _helptext: Text;
-    private _dotplottext: Text;
-    private _meltplottext: Text;
-    private _coordBalloon: TextBalloon;
 
     private _dotplotScaleLevel: number;
-    private _zoom_in: GameButton;
-    private _zoom_out: GameButton;
 
     private _drag: boolean;
     private _dragBeginX: number;
@@ -433,7 +463,8 @@ export class SpecBox extends GamePanel {
     private _dotplotOriginX: number;
     private _dotplotOriginY: number;
 
-    private readonly _docked: boolean;
+    private _width: number = 0;
+    private _height: number = 0;
 
     private static readonly HORIZONTAL: number = 0;
     // this recalculate position of gametext at index th gametext
