50193a166d8d7c29f8ebed63a746088a77e886e7
Tim Conkling <tconkling@gmail.com>
2018-07-10 13:45:18 -0400

    MissionScreen -> MissionIntroMode; some updates

--- src/eterna/mode/PoseEdit/MissionIntroMode.ts
+++ src/eterna/mode/PoseEdit/MissionIntroMode.ts
@@ -1,17 +1,16 @@
-import {Graphics, Sprite} from "pixi.js";
+import {Graphics, Point, Sprite} from "pixi.js";
 import {AppMode} from "../../../flashbang/core/AppMode";
 import {Flashbang} from "../../../flashbang/core/Flashbang";
-import {GameObject} from "../../../flashbang/core/GameObject";
 import {DisplayObjectPointerTarget} from "../../../flashbang/input/DisplayObjectPointerTarget";
-import {DisplayUtil} from "../../../flashbang/util/DisplayUtil";
+import {KeyCode} from "../../../flashbang/input/KeyCode";
 import {EPars} from "../../EPars";
 import {BitmapManager} from "../../resources/BitmapManager";
 import {GameButton} from "../../ui/GameButton";
 import {PoseThumbnail} from "../../ui/PoseThumbnail";
 import {Fonts} from "../../util/Fonts";
-import {UDim} from "../../util/UDim";
+import Container = PIXI.Container;
 
-export class MissionScreen extends AppMode {
+export class MissionIntroMode extends AppMode {
     public constructor(puzzleName: string, puzzleDescription: string, puzzleThumbnails: number[][]) {
         super();
 
@@ -24,65 +23,84 @@ export class MissionScreen extends AppMode {
 
         let molecule_background: Sprite = Sprite.fromImage(BitmapManager.MissionBackgroundImage);
         this.modeSprite.addChild(molecule_background);
-        new UDim(0.5, 0, -1 * DisplayUtil.width(molecule_background) / 2, 0).setPos(molecule_background);
+        molecule_background.position = new Point(
+            (Flashbang.stageWidth - molecule_background.width) * 0.5,
+            0);
 
         let mission_text = Fonts.std_light("MISSION", 48).color(0xFFCC00).build();
         this.modeSprite.addChild(mission_text);
-        new UDim(0.5, 0, -420.5, 123).setPos(mission_text);
+        mission_text.position = new Point(
+            (Flashbang.stageWidth * 0.5) - 420.5,
+            123);
 
         let description_label = Fonts.std_light(puzzleDescription).color(0xBCD8E3).fontSize(36).leading(50).build();
         this.modeSprite.addChild(description_label);
-        new UDim(0.5, 0, -420.5, 123 + mission_text.height + 25).setPos(description_label);
+        description_label.position = new Point(
+            (Flashbang.stageWidth * 0.5) - 420.5,
+            123 + mission_text.height + 25);
 
         let play_button: GameButton = new GameButton()
             .up(BitmapManager.PlayImage)
             .over(BitmapManager.PlayImageOver)
             .down(BitmapManager.PlayImageHit);
         this.addObject(play_button, this.modeSprite);
-        new UDim(1, 1, -DisplayUtil.width(play_button.display)- 91.5, -30 - DisplayUtil.height(play_button.display)).setPos(play_button.display);
+        play_button.display.position = new Point(
+            Flashbang.stageWidth - play_button.container.width - 91.5,
+            Flashbang.stageHeight - 30 - play_button.container.height);
         this.regs.add(play_button.clicked.connect(() => this.play()));
 
         let bgImage: Sprite = Sprite.fromImage(BitmapManager.MissionPuzzleIdImage);
         this.modeSprite.addChild(bgImage);
-        new UDim(0.5, 0, -420.5, 0).setPos(bgImage);
-
+        bgImage.position = new Point((Flashbang.stageWidth * 0.5) - 420.5, 0);
 
         let name_label = Fonts.std_light(puzzleName, 18).color(0xFFFFFF).letterSpacing(0).build();
         this.modeSprite.addChild(name_label);
-        new UDim(0.5, 0, -420.5 + MissionScreen.PUZZLE_LABEL_X_MARGIN, MissionScreen.PUZZLE_LABEL_Y_MARGIN).setPos(name_label);
+        name_label.position = new Point(
+            (Flashbang.stageWidth * 0.5) - 420.5 + MissionIntroMode.PUZZLE_LABEL_X_MARGIN,
+            MissionIntroMode.PUZZLE_LABEL_Y_MARGIN);
         let real_width: number = name_label.width;
         if (bgImage) {
-            bgImage.width = real_width + MissionScreen.PUZZLE_LABEL_X_MARGIN * 2;
+            bgImage.width = real_width + MissionIntroMode.PUZZLE_LABEL_X_MARGIN * 2;
         }
 
         let goals_label = Fonts.std_light("GOAL", 24).color(0xffcc00).build();
         this.modeSprite.addChild(goals_label);
-        new UDim(0.5, 0, -420.5, 367 + 15).setPos(goals_label);
+        goals_label.position = new Point(
+            (Flashbang.stageWidth * 0.5) - 420.5,
+            367 + 15);
 
         this._goals_background = Sprite.fromImage(BitmapManager.MissionPuzzleThumbnailImage);
         this.modeSprite.addChild(this._goals_background);
-        new UDim(0.5, 0, -420.5, 367 + 60).setPos(this._goals_background);
+        this._goals_background.position = new Point(
+            (Flashbang.stageWidth * 0.5) - 420.5,
+            367 + 60);
 
         this._goals_thumbnail = new Sprite();
         this.modeSprite.addChild(this._goals_thumbnail);
-        new UDim(0.5, 0, -420.5 + 22.5, 367 + 60 + 22.5).setPos(this._goals_thumbnail);
+        this._goals_thumbnail.position = new Point(
+            (Flashbang.stageWidth * 0.5) - 420.5 + 22.5,
+            367 + 60 + 22.5);
 
-        // this._scroll_up_button = new GameButton(22, BitmapUtil.scale_by(BitmapManager.get_bitmap(BitmapManager.ImgUpArrow), 0.15));
-        // this._scroll_up_button.set_click_callback(this.scroll_up);
-        // this._scroll_up_button.set_pos(new UDim(0.5, 0, 420.5 - this._scroll_up_button.width - 30, 367 + 40));
-        // this._scroll_up_button.visible = false;
-        // this._scroll_up_button.set_hotkey(KeyCode.KEY_UP, false, "up");
-        // this.addObject(this._scroll_up_button);
-        //
-        // this._scroll_down_button = new GameButton(22, BitmapUtil.scale_by(BitmapManager.get_bitmap(BitmapManager.ImgDownArrow), 0.15));
-        // this._scroll_down_button.set_click_callback(this.scroll_down);
-        // this._scroll_down_button.set_pos(new UDim(0.5, 1, 420.5 - this._scroll_down_button.width - 30, -55 - play_button.height - this._scroll_down_button.height - 15));
-        // this._scroll_down_button.visible = false;
-        // this._scroll_down_button.set_hotkey(KeyCode.KEY_DOWN, false, "down");
-        // this.addObject(this._scroll_down_button);
+        this._scroll_up_button = new GameButton().allStates(BitmapManager.ImgUpArrow).hotkey(KeyCode.ArrowUp);
+        this._scroll_up_button.display.scale = new Point(0.15, 0.15);
+        this._scroll_up_button.display.visible = false;
+        this._scroll_up_button.display.position = new Point(
+            (Flashbang.stageWidth * 0.5) + 420.5 - this._scroll_up_button.container.width - 30,
+            367 + 40);
+        this._scroll_up_button.clicked.connect(() => this.scroll_up());
+        this.addObject(this._scroll_up_button, this.modeSprite);
 
-        this._constraints_container = new GameObject;
-        this.addObject(this._constraints_container);
+        this._scroll_down_button = new GameButton().allStates(BitmapManager.ImgDownArrow).hotkey(KeyCode.ArrowDown);
+        this._scroll_down_button.display.scale = new Point(0.15, 0.15);
+        this._scroll_down_button.display.visible = false;
+        this._scroll_down_button.display.position = new Point(
+            (Flashbang.stageWidth * 0.5) + 420.5 - this._scroll_down_button.container.width - 30,
+            Flashbang.stageHeight - 55 - play_button.container.height - this._scroll_down_button.container.height - 15);
+        this._scroll_down_button.clicked.connect(() => this.scroll_down());
+        this.addObject(this._scroll_down_button, this.modeSprite);
+
+        this._constraintsLayer = new Container();
+        this.modeSprite.addChild(this._constraintsLayer);
         this.setup_constraint_masks();
 
         this.set_puzzle_thumbnails(puzzleThumbnails);
@@ -217,7 +235,7 @@ export class MissionScreen extends AppMode {
         //     bot_position.get_y(Flashbang.stageHeight) + this._scroll_down_button.height - top_position.get_y(Flashbang.stageHeight));
         // cc_mask.x = 0;
         // cc_mask.y = 0;
-        // this._constraints_container.mask = cc_mask;
+        // this._constraintsLayer.mask = cc_mask;
     }
 
     private readonly _background: Graphics;
@@ -228,7 +246,7 @@ export class MissionScreen extends AppMode {
     private _goals_targets: number[][];
     private _current_target: number;
 
-    private readonly _constraints_container: GameObject;
+    private readonly _constraintsLayer: Container;
     private _constraints: any[];
     private _constraint_height: number = 0.0;
 
