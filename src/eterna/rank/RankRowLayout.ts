b6bfd6afe370d3bd9514273172961345ec58c299
Tim Conkling <tconkling@gmail.com>
2018-07-12 13:49:17 -0400

    RankRowLayout

---
+++ src/eterna/rank/RankRowLayout.ts
@@ -0,0 +1,69 @@
+import {Container, Point, Text} from "pixi.js";
+import {TextUtil} from "../../flashbang/util/TextUtil";
+import {Fonts} from "../util/Fonts";
+import {PlayerRank} from "./PlayerRank";
+
+export class RankRowLayout extends Container {
+    public constructor(rank: number, data: PlayerRank, offset_btw_rank_coin: number,
+                       fontSize: number = 18, maxTextWidth: number = -1, textColor: number = 0xFFFFFF) {
+        super();
+
+        this._tfName = Fonts.std_regular("", fontSize).color(textColor).build();
+        this.addChild(this._tfName);
+
+        this._tfRank = Fonts.std_regular("", fontSize).color(textColor).build();
+        this._tfRank.position = new Point(130, 0);
+        this.addChild(this._tfRank);
+
+        this._tfCoin = Fonts.std_regular("", fontSize).color(textColor).build();
+        this._tfCoin.position = new Point(130 + offset_btw_rank_coin, 0);
+        this.addChild(this._tfCoin);
+
+        this.set_player_name(data.name);
+        this.set_rank(rank);
+        if (maxTextWidth != -1) {
+            this.trim_name_to_pixel(maxTextWidth);
+        }
+        this.set_coin(data.score);
+    }
+
+    public set_rank(rank: number): void {
+        this._tfRank.text = rank < 0 ? "" : "" + rank;
+        this._rank = rank;
+    }
+
+    public get_rank(): number {
+        return this._rank;
+    }
+
+    public set_player_name(player_name: string): void {
+        this._player_name = player_name;
+        this._tfName.text = player_name;
+    }
+
+    public trim_name_to_pixel(maxTextWidth: number): void {
+        TextUtil.trimTextToWidth(this._tfName, maxTextWidth, "...");
+        this._player_name = this._tfName.text;
+    }
+
+    public get_player_name(): string {
+        return this._player_name;
+    }
+
+    public set_coin(coin: number): void {
+        this._tfCoin.text = (coin < 0) ? "" : coin.toString();
+        this._coin = coin;
+    }
+
+    public get_coin(): number {
+        return this._coin;
+    }
+
+    private readonly _tfRank: Text;
+    private readonly _tfName: Text;
+    private readonly _tfCoin: Text;
+
+    private _rank: number;
+    private _player_name: string;
+    private _coin: number;
+}
---
+++ src/flashbang/util/TextUtil.ts
@@ -0,0 +1,29 @@
+import {Text} from "pixi.js";
+
+export class TextUtil {
+    /**
+     * Trims the given text field to fit into the given max width, if possible.
+     * If trimmedPostfix is non-null, and the textfield must be trimmed, the
+     * postfix will be added to the trimmed text.
+     */
+    public static trimTextToWidth(text: Text, maxWidth: number, trimmedPostfix: string = null): void {
+        if (text.width <= maxWidth) {
+            return;
+        }
+
+        trimmedPostfix = trimmedPostfix || "";
+
+        const originalString = text.text;
+        const originalWidth = text.width;
+        text.text += trimmedPostfix;
+        const postfixWidth = text.width - originalWidth;
+        text.text = originalString;
+
+        maxWidth = Math.max(maxWidth - postfixWidth, 0);
+        while (text.text.length > 0 && text.width > maxWidth) {
+            text.text = text.text.substr(0, text.text.length - 1);
+        }
+
+        text.text += trimmedPostfix;
+    }
+}
