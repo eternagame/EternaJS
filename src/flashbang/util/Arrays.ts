784c720b9d5dacfa972fc8596f5fa5b68c84dc06
Tim Conkling <tconkling@gmail.com>
2018-07-17 13:00:53 -0700

    Arrays.shallowEqual util

---
+++ src/flashbang/util/Arrays.ts
@@ -0,0 +1,18 @@
+export class Arrays {
+    /** True if two Arrays hold the same contents */
+    public static shallowEqual<T>(a: T[], b: T[]): boolean {
+        if (a === b) {
+            return true;
+        } else if (a == null || b == null || a.length != b.length) {
+            return false;
+        }
+
+        for (let ii = 0; ii < a.length; ++ii) {
+            if (a[ii] != b[ii]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
