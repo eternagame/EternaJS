2255849441d7749570a71e7c8650cecb7b77d589
Tim Conkling <tconkling@gmail.com>
2018-07-18 13:10:24 -0700

    AbstractSettings: support for storing arbitrary objects
    Also, disallow setting name clashes

--- src/flashbang/settings/AbstractSettings.ts
+++ src/flashbang/settings/AbstractSettings.ts
@@ -11,7 +11,31 @@ export abstract class AbstractSettings {
         this._namespace = storejs.namespace(namespace);
     }
 
+    /** Saves an object to this Settings namespace */
+    public saveObject(name: string, obj: any): void {
+        if (this._allSettings.has(name)) {
+            throw new Error(`'${name}' is used by a setting`);
+        }
+
+        this._namespace.set(name, obj);
+    }
+
+    /** Loads an object from this Settings, if it exists */
+    public loadObject(name: string): any {
+        return this._namespace.get(name);
+    }
+
+    /** Deletes an object from this Settings, if it exists */
+    public removeObject(name: string): void {
+        this._namespace.remove(name);
+    }
+
     protected setting<T>(name: string, defaultVal: T): Setting<T> {
+        if (this._allSettings.has(name)) {
+            throw new Error(`Setting '${name}' already taken`);
+        }
+        this._allSettings.add(name);
+
         return new Setting<T>(this._namespace, name, defaultVal);
     }
 
@@ -20,6 +44,7 @@ export abstract class AbstractSettings {
     }
 
     protected readonly _namespace: StoreJsAPI;
+    protected readonly _allSettings: Set<string> = new Set();
 
     protected static readonly ALL_NAMESPACES: Set<string> = new Set();
 }
