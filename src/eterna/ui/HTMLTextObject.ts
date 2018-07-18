a8cab802d2d5b40db1d4e27dc0c4dfc6d86ac80f
Tim Conkling <tconkling@gmail.com>
2018-07-17 18:31:05 -0700

    HTMLTextObject improvements

--- src/eterna/ui/HTMLTextObject.ts
+++ src/eterna/ui/HTMLTextObject.ts
@@ -1,44 +1,54 @@
 import {DOMObject} from "../../flashbang/objects/DOMObject";
 import {Eterna} from "../Eterna";
 
-/** A <p> object in the DOM. Floats on top of the PIXI canvas. */
+/** A <p> object in the DOM that contains the given HTML. Floats on top of the PIXI canvas. */
 export class HTMLTextObject extends DOMObject<HTMLParagraphElement> {
-    public constructor(htmlText: string, width: number = 100) {
+    public constructor(htmlText: string, width?: number) {
         super(Eterna.OVERLAY_DIV_ID, document.createElement("p"));
-        this.width = width;
         this._obj.innerHTML = htmlText;
+        this._obj.style.margin = "0px";
+        if (width) {
+            this.width = width;
+        } else {
+            this._obj.style.width = "max-content";
+        }
     }
 
     public lineHeight(percentOrString: number | string): HTMLTextObject {
-        if (typeof(percentOrString) === "number") {
-            this._obj.style.lineHeight = `${Math.floor(percentOrString * 100)}%`;
-        } else {
-            this._obj.style.lineHeight = percentOrString;
-        }
+        let lineHeight: string = (typeof(percentOrString) === "number") ?
+            `${Math.floor(percentOrString * 100)}%` :
+            percentOrString;
+        DOMObject.applyStyleRecursive(this._obj, "line-height", lineHeight);
         this.onSizeChanged();
         return this;
     }
 
     public color(color: number): HTMLTextObject {
-        this._obj.style.color = `#${color.toString(16)}`;
+        DOMObject.applyStyleRecursive(this._obj, "color", `#${color.toString(16)}`);
         this.onSizeChanged();
         return this;
     }
 
     public font(fontFamily: string): HTMLTextObject {
-        this._obj.style.fontFamily = fontFamily;
+        DOMObject.applyStyleRecursive(this._obj, "font-family", fontFamily);
         this.onSizeChanged();
         return this;
     }
 
     public fontSize(size: number): HTMLTextObject {
-        this._obj.style.fontSize = `${size}pt`;
+        DOMObject.applyStyleRecursive(this._obj, "font-size", `${size}pt`);
         this.onSizeChanged();
         return this;
     }
 
     public hAlign(align: "left" | "center" | "right" | "justify"): HTMLTextObject {
-        this._obj.style.textAlign = align;
+        DOMObject.applyStyleRecursive(this._obj, "text-align", align);
+        this.onSizeChanged();
+        return this;
+    }
+
+    public maxWidth(value: number): HTMLTextObject {
+        this._obj.style.maxWidth = DOMObject.sizeToString(value);
         this.onSizeChanged();
         return this;
     }
