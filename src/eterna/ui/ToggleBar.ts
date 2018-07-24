900f4af141d28d8a492ea0a501eebaccd3f5ff28
Tim Conkling <tconkling@gmail.com>
2018-07-24 13:16:42 -0700

    ToggleBar: don't respond to mouseMove while mouse is not over

--- src/eterna/ui/ToggleBar.ts
+++ src/eterna/ui/ToggleBar.ts
@@ -20,7 +20,7 @@ export class ToggleBar extends ContainerObject implements KeyboardListener {
     constructor(num_states: number) {
         super();
 
-        this._num_states = num_states;
+        this._numStates = num_states;
 
         this._bg = new Graphics();
         this.container.addChild(this._bg);
@@ -28,27 +28,27 @@ export class ToggleBar extends ContainerObject implements KeyboardListener {
         this._bg.clear();
         this._bg.beginFill(ToggleBar.COLOR_DARK, 0.6);
         this._bg.lineStyle(2, ToggleBar.COLOR_LIGHT, 0.85);
-        this._bg.drawRoundedRect(0, 0, ToggleBar.BUTTON_SIZE * this._num_states, ToggleBar.BUTTON_SIZE, ToggleBar.ROUND_RECT_RADIUS);
+        this._bg.drawRoundedRect(0, 0, ToggleBar.BUTTON_SIZE * this._numStates, ToggleBar.BUTTON_SIZE, ToggleBar.ROUND_RECT_RADIUS);
         this._bg.endFill();
 
-        this._bg_active = new Graphics();
-        this.container.addChild(this._bg_active);
+        this._hoverHilite = new Graphics();
+        this.container.addChild(this._hoverHilite);
 
-        this._bg_active.clear();
-        this._bg_active.beginFill(ToggleBar.COLOR_MEDIUM, 0.45);
-        this._bg_active.drawRoundedRect(0, 0, ToggleBar.BUTTON_SIZE, ToggleBar.BUTTON_SIZE, ToggleBar.ROUND_RECT_RADIUS);
-        this._bg_active.endFill();
-        this._bg_active.visible = false;
+        this._hoverHilite.clear();
+        this._hoverHilite.beginFill(ToggleBar.COLOR_MEDIUM, 0.45);
+        this._hoverHilite.drawRoundedRect(0, 0, ToggleBar.BUTTON_SIZE, ToggleBar.BUTTON_SIZE, ToggleBar.ROUND_RECT_RADIUS);
+        this._hoverHilite.endFill();
+        this._hoverHilite.visible = false;
 
-        this._bg_selected = new Graphics();
-        this.container.addChild(this._bg_selected);
+        this._selectedHilite = new Graphics();
+        this.container.addChild(this._selectedHilite);
 
-        this._bg_selected.clear();
-        this._bg_selected.beginFill(ToggleBar.COLOR_LIGHT, 0.85);
-        this._bg_selected.drawRoundedRect(0, 0, ToggleBar.BUTTON_SIZE, ToggleBar.BUTTON_SIZE, ToggleBar.ROUND_RECT_RADIUS);
-        this._bg_selected.endFill();
+        this._selectedHilite.clear();
+        this._selectedHilite.beginFill(ToggleBar.COLOR_LIGHT, 0.85);
+        this._selectedHilite.drawRoundedRect(0, 0, ToggleBar.BUTTON_SIZE, ToggleBar.BUTTON_SIZE, ToggleBar.ROUND_RECT_RADIUS);
+        this._selectedHilite.endFill();
 
-        for (let ii = 0; ii < this._num_states; ii++) {
+        for (let ii = 0; ii < this._numStates; ii++) {
             this._labels[ii] = Fonts.arial("" + (ii + 1), 12).color(ToggleBar.COLOR_TEXT).build();
             this._labels[ii].position = new Point((ii * ToggleBar.BUTTON_SIZE) + 9, 5);
             this.container.addChild(this._labels[ii]);
@@ -57,9 +57,10 @@ export class ToggleBar extends ContainerObject implements KeyboardListener {
         this.set_state(0);
 
         this.display.interactive = true;
-        this.pointerTap.connect((event) => this.on_click(event));
-        this.pointerOut.connect((event) => this.on_mouse_out(event));
-        this.pointerMove.connect((event) => this.on_mouse_move(event));
+        this.pointerOver.connect(() => this.onMouseOver());
+        this.pointerOut.connect(() => this.onMouseOut());
+        this.pointerTap.connect((event) => this.onMouseClick(event));
+        this.pointerMove.connect((event) => this.onMouseMove(event));
     }
 
     protected added(): void {
@@ -72,15 +73,15 @@ export class ToggleBar extends ContainerObject implements KeyboardListener {
     }
 
     public set_state(new_state: number): void {
-        if (new_state != this._current_state) {
-            if ((this._current_state >= 0) && (this._current_state < this._num_states)) {
-                this._labels[this._current_state].style.fill = ToggleBar.COLOR_TEXT;
+        if (new_state != this._selectedState) {
+            if ((this._selectedState >= 0) && (this._selectedState < this._numStates)) {
+                this._labels[this._selectedState].style.fill = ToggleBar.COLOR_TEXT;
             }
 
-            this._current_state = new_state;
+            this._selectedState = new_state;
             this.replaceNamedObject("BGSelectedAnim",
-                new LocationTask(this._current_state * ToggleBar.BUTTON_SIZE, 0, 0.5, Easing.easeInOut, this._bg_selected));
-            this._labels[this._current_state].style.fill = ToggleBar.COLOR_HIGH;
+                new LocationTask(this._selectedState * ToggleBar.BUTTON_SIZE, 0, 0.5, Easing.easeInOut, this._selectedHilite));
+            this._labels[this._selectedState].style.fill = ToggleBar.COLOR_HIGH;
 
             Eterna.sound.play_se(Sounds.SoundSwitch);
             this.stateChanged.emit(new_state);
@@ -93,7 +94,7 @@ export class ToggleBar extends ContainerObject implements KeyboardListener {
         }
 
         if (e.type == KeyboardEventType.KEY_DOWN && e.code == KeyCode.Tab && !e.ctrlKey) {
-            this.set_state((this._current_state + 1) % this._num_states);
+            this.set_state((this._selectedState + 1) % this._numStates);
             e.preventDefault(); // prevent Tab from changing focus in the browser
             return true;
         } else {
@@ -106,9 +107,9 @@ export class ToggleBar extends ContainerObject implements KeyboardListener {
         this.display.alpha = disabled ? 0.3 : 1.0;
     }
 
-    private on_click(e: InteractionEvent): void {
-        let state: number = this.getClickedState(e);
-        if ((state == this._current_state) || (state < 0) || (state >= this._num_states)) {
+    private onMouseClick(e: InteractionEvent): void {
+        let state: number = this.getStateUnderMouse(e);
+        if ((state == this._selectedState) || (state < 0) || (state >= this._numStates)) {
             return;
         }
 
@@ -116,49 +117,58 @@ export class ToggleBar extends ContainerObject implements KeyboardListener {
         ROPWait.NotifyClickUI("SWITCH");
     }
 
-    private on_mouse_out(e: InteractionEvent): void {
-        this._bg_active.visible = false;
-        if ((this._current_over >= 0) && (this._current_over != this._current_state)) {
-            this._labels[this._current_over].style.fill = ToggleBar.COLOR_TEXT;
-        }
-        this._current_over = -1;
+    private onMouseOver(): void {
+        this._mouseOver = true;
     }
 
-    private on_mouse_move(e: InteractionEvent): void {
-        let state: number = this.getClickedState(e);
-        if ((state == this._current_over) || (state < 0) || (state >= this._num_states)) {
+    private onMouseOut(): void {
+        this._mouseOver = false;
+        this._hoverHilite.visible = false;
+        if ((this._hoveredState >= 0) && (this._hoveredState != this._selectedState)) {
+            this._labels[this._hoveredState].style.fill = ToggleBar.COLOR_TEXT;
+        }
+        this._hoveredState = -1;
+    }
+
+    private onMouseMove(e: InteractionEvent): void {
+        if (!this._mouseOver) {
             return;
         }
 
-        if (this._current_over >= 0 && this._current_over != this._current_state) {
-            this._labels[this._current_over].style.fill = ToggleBar.COLOR_TEXT;
-        }
-
-        this._current_over = state;
-        if (this._current_over == this._current_state) {
-            this._bg_active.visible = false;
+        let state: number = this.getStateUnderMouse(e);
+        if ((state == this._hoveredState) || (state < 0) || (state >= this._numStates)) {
             return;
         }
 
-        this._bg_active.visible = true;
-        this._bg_active.position = new Point(this._current_over * ToggleBar.BUTTON_SIZE, 0);
-        this._labels[this._current_over].style.fill = ToggleBar.COLOR_HIGH;
+        if (this._hoveredState >= 0 && this._hoveredState != this._selectedState) {
+            this._labels[this._hoveredState].style.fill = ToggleBar.COLOR_TEXT;
+        }
+
+        this._hoveredState = state;
+        if (this._hoveredState == this._selectedState) {
+            this._hoverHilite.visible = false;
+        } else {
+            this._hoverHilite.visible = true;
+            this._hoverHilite.position = new Point(this._hoveredState * ToggleBar.BUTTON_SIZE, 0);
+            this._labels[this._hoveredState].style.fill = ToggleBar.COLOR_HIGH;
+        }
     }
 
-    private getClickedState(e: InteractionEvent): number {
+    private getStateUnderMouse(e: InteractionEvent): number {
         e.data.getLocalPosition(this.display, ToggleBar.P);
         return Math.floor(ToggleBar.P.x / ToggleBar.BUTTON_SIZE);
     }
 
     private readonly _bg: Graphics;
-    private readonly _bg_active: Graphics;
-    private readonly _bg_selected: Graphics;
+    private readonly _hoverHilite: Graphics;
+    private readonly _selectedHilite: Graphics;
 
-    private readonly _num_states: number;
+    private readonly _numStates: number;
 
     private _disabled: boolean = false;
-    private _current_state: number = -1;
-    private _current_over: number = -1;
+    private _selectedState: number = -1;
+    private _hoveredState: number = -1;
+    private _mouseOver: boolean = false;
     private _labels: Text[] = [];
     private _rscript_name: string = "";
 
