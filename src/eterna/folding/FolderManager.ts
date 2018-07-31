543850308b23a41f824cb0c636507f6acf788a73
Tim Conkling <tconkling@gmail.com>
2018-07-31 12:18:39 -0700

    FolderManager fixes

--- src/eterna/folding/FolderManager.ts
+++ src/eterna/folding/FolderManager.ts
@@ -21,31 +21,30 @@ export class FolderManager {
     }
 
     public get_folder(name: string): Folder {
-        let folder: Folder;
-        for (folder of this._folders) {
+        for (let folder of this._folders) {
             if (folder.get_folder_name() == name) {
                 return folder;
             }
         }
 
-        throw new Error("Trying to query non-existing folder ('" + folder.get_folder_name() + "')");
+        throw new Error(`No such folder '${name}'`);
     }
 
     public get_next_folder(folder_name: string, filter_cb: Function = null): Folder {
-        let ii: number;
-        for (ii = 0; ii < this._folders.length; ii++) {
+        let curFolderIdx: number = -1;
+        for (let ii = 0; ii < this._folders.length; ii++) {
             if (this._folders[ii].get_folder_name() == folder_name) {
+                curFolderIdx = ii;
                 break;
             }
         }
 
-        if (ii >= this._folders.length) {
-            throw new Error("Folder '" + folder_name + "' doesn't exist");
+        if (curFolderIdx < 0) {
+            throw new Error(`No such folder '${folder_name}'`);
         }
 
-        let jj: number;
-        for (jj = 1; jj < this._folders.length; jj++) {
-            let idx: number = (ii + jj) % this._folders.length;
+        for (let jj = 1; jj < this._folders.length; jj++) {
+            let idx: number = (curFolderIdx + jj) % this._folders.length;
             let folder: Folder = this._folders[idx];
 
             if (folder.get_folder_name().length == 0 ||
@@ -59,7 +58,7 @@ export class FolderManager {
             return folder;
         }
 
-        return this._folders[ii]; // use same one
+        return this._folders[curFolderIdx]; // use same one
     }
 
     public get_last_used_folder(): string {
