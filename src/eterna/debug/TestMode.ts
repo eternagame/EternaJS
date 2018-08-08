de1deacd9c0b0fd966b1ec0be767ba0e77518354
Tim Conkling <tconkling@gmail.com>
2018-08-08 16:53:33 -0700

    ExternalInterface

--- src/eterna/debug/TestMode.ts
+++ src/eterna/debug/TestMode.ts
@@ -1,6 +1,11 @@
 import {AppMode} from "../../flashbang/core/AppMode";
 import {Flashbang} from "../../flashbang/core/Flashbang";
+import {AlphaTask} from "../../flashbang/tasks/AlphaTask";
+import {RepeatingTask} from "../../flashbang/tasks/RepeatingTask";
+import {SerialTask} from "../../flashbang/tasks/SerialTask";
+import {Easing} from "../../flashbang/util/Easing";
 import {StyledTextBuilder} from "../../flashbang/util/StyledTextBuilder";
+import {ExternalInterface} from "../util/ExternalInterface";
 import {Background} from "../vfx/Background";
 
 export class TestMode extends AppMode {
@@ -19,5 +24,25 @@ export class TestMode extends AppMode {
         tf.x = (Flashbang.stageWidth - tf.width) * 0.5;
         tf.y = (Flashbang.stageHeight - tf.height) * 0.5;
         this.modeSprite.addChild(tf);
+
+        ExternalInterface.addCallback("Animate", (time?: number) => {
+            if (this.hasNamedObject("Animate")) {
+                this.removeNamedObjects("Animate");
+                return "Stopped!";
+            } else {
+                if (time === undefined) {
+                    time = 0.25;
+                }
+
+                this.addNamedObject("Animate", new RepeatingTask(() => {
+                    return new SerialTask(
+                        new AlphaTask(0, time, Easing.easeInOut, tf),
+                        new AlphaTask(1, time, Easing.easeInOut, tf),
+                    );
+                }));
+            }
+
+            return "Started!";
+        });
     }
 }
