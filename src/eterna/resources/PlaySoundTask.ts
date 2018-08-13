85be507ba37b33fdebbbc4ab61e4b91bd1bc9982
Tim Conkling <tconkling@gmail.com>
2018-08-13 14:41:45 -0700

    PlaySoundTask

---
+++ src/eterna/resources/PlaySoundTask.ts
@@ -0,0 +1,16 @@
+import {ObjectTask} from "../../flashbang/core/ObjectTask";
+import {Eterna} from "../Eterna";
+
+export class PlaySoundTask extends ObjectTask {
+    public constructor(name: string) {
+        super();
+        this._soundName = name;
+    }
+
+    protected added(): void {
+        Eterna.sound.play_se(this._soundName);
+        this.destroySelf();
+    }
+
+    private readonly _soundName: string;
+}
