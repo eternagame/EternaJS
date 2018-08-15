e51f183bad55cc80133c52c31ec33f00bc62d424
Tim Conkling <tconkling@gmail.com>
2018-08-15 14:03:20 -0700

    whoops, fix postbuild:surge script

--- src/eterna/EternaApp.ts
+++ src/eterna/EternaApp.ts
@@ -162,7 +162,7 @@ export class EternaApp extends FlashbangApp {
 
     private readonly _width: number = 1280;
     private readonly _height: number = 1024;
-    private readonly _puzzleID: number = PuzzleID.Tutorial1;
+    private readonly _puzzleID: number = PuzzleID.TheophyllineRibozymeSwitch;
 
     private static readonly PIXI_CONTAINER_ID = "pixi-container";
 }
