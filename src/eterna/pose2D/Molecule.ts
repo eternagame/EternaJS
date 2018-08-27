4ee69549d941639e8f669d9ded9118a8b09d89f3
Tim Conkling <tconkling@gmail.com>
2018-08-27 16:20:08 -0700

    Molecule: camelCase

--- src/eterna/pose2D/Molecule.ts
+++ src/eterna/pose2D/Molecule.ts
@@ -6,60 +6,60 @@ import {BitmapUtil} from "../util/BitmapUtil";
 export class Molecule extends Container {
     public constructor() {
         super();
-        Molecule.initBitmapData();
+        Molecule.initTextures();
 
         this.addChild(this._glow);
         this.addChild(this._body);
     }
 
-    public set_wrong(wrong: boolean): void {
-        this._is_wrong = wrong;
+    public set isWrong(wrong: boolean) {
+        this._isWrong = wrong;
     }
 
-    public get_last_drawn_pos(): Point {
-        return new Point(this._last_drawn_x, this._last_drawn_y);
+    public getLastDrawnPos(): Point {
+        return new Point(this._lastDrawnX, this._lastDrawnY);
     }
 
     public updateView(zoom_level: number, x: number, y: number, current_time: number): void {
-        if (this._animation_start_time < 0) {
-            this._animation_start_time = current_time;
+        if (this._animStartTime < 0) {
+            this._animStartTime = current_time;
         }
 
-        let diff: number = current_time - this._animation_start_time;
+        let diff: number = current_time - this._animStartTime;
         let prog: number = Math.sin(diff * Math.PI / (Molecule.ANIMATION_SPAN)) + 0.95;
         prog = Math.max(0, prog);
         let prog_ind: number = Math.floor(prog / 2.0 * Molecule.NUM_ANIMATION_STEPS) % Molecule.NUM_ANIMATION_STEPS;
 
-        let glow_data: Texture = this._is_wrong ?
-            Molecule._glow_wrong_data[zoom_level][prog_ind] :
-            Molecule._glow_data[zoom_level][prog_ind];
+        let glow_data: Texture = this._isWrong ?
+            Molecule._glowWrongTex[zoom_level][prog_ind] :
+            Molecule._glowTex[zoom_level][prog_ind];
 
         this._glow.texture = glow_data;
         this._glow.pivot.x = glow_data.width * 0.5;
         this._glow.pivot.y = glow_data.height * 0.5;
 
         prog_ind = Math.floor(diff / Molecule.BODY_ANIMATION_SPAN * Molecule.NUM_ANIMATION_STEPS) % Molecule.NUM_ANIMATION_STEPS;
-        let body_data: Texture = Molecule._body_data[zoom_level][prog_ind];
+        let body_data: Texture = Molecule._bodyTex[zoom_level][prog_ind];
 
         this._body.texture = body_data;
         this._body.pivot.x = body_data.width * 0.5;
         this._body.pivot.y = body_data.height * 0.5;
 
-        this._last_drawn_x = x;
-        this._last_drawn_y = y;
+        this._lastDrawnX = x;
+        this._lastDrawnY = y;
 
         this.x = x;
         this.y = y;
     }
 
-    private static initBitmapData(): void {
-        if (Molecule._glow_data != null) {
+    private static initTextures(): void {
+        if (Molecule._glowTex != null) {
             return;
         }
 
-        Molecule._glow_data = [];
-        Molecule._glow_wrong_data = [];
-        Molecule._body_data = [];
+        Molecule._glowTex = [];
+        Molecule._glowWrongTex = [];
+        Molecule._bodyTex = [];
 
         let original_glow_data: Texture = BitmapManager.get_bitmap(Bitmaps.ImgMoleculeOuter);
         let original_body_data: Texture = BitmapManager.get_bitmap(Bitmaps.ImgMoleculeInner);
@@ -85,23 +85,23 @@ export class Molecule extends Container {
                 body_bitmaps_in_zoom.push(body_data);
             }
 
-            Molecule._glow_data.push(bitmaps_in_zoom);
-            Molecule._glow_wrong_data.push(wrong_bitmaps_in_zoom);
-            Molecule._body_data.push(body_bitmaps_in_zoom);
+            Molecule._glowTex.push(bitmaps_in_zoom);
+            Molecule._glowWrongTex.push(wrong_bitmaps_in_zoom);
+            Molecule._bodyTex.push(body_bitmaps_in_zoom);
         }
     }
 
     private readonly _glow: Sprite = new Sprite();
     private readonly _body: Sprite = new Sprite();
 
-    private _animation_start_time: number = -1;
-    private _is_wrong: boolean = false;
-    private _last_drawn_y: number = 0;
-    private _last_drawn_x: number = 0;
+    private _animStartTime: number = -1;
+    private _isWrong: boolean = false;
+    private _lastDrawnY: number = 0;
+    private _lastDrawnX: number = 0;
 
-    private static _glow_data: Texture[][];
-    private static _glow_wrong_data: Texture[][];
-    private static _body_data: Texture[][];
+    private static _glowTex: Texture[][];
+    private static _glowWrongTex: Texture[][];
+    private static _bodyTex: Texture[][];
 
     private static readonly NUM_ANIMATION_STEPS: number = 60;
     private static readonly ANIMATION_SPAN: number = 1.1;
