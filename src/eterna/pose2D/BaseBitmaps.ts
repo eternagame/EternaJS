e5883f12dca16159f79435b2e964cf4a4e125fcb
Tim Conkling <tconkling@gmail.com>
2018-08-27 16:15:37 -0700

    BaseBitmaps: camelCase

--- src/eterna/pose2D/BaseBitmaps.ts
+++ src/eterna/pose2D/BaseBitmaps.ts
@@ -1,8 +1,7 @@
-import {Texture, Text} from "pixi.js";
+import {Text, Texture} from "pixi.js";
 import {TextureUtil} from "../../flashbang/util/TextureUtil";
 import {EPars} from "../EPars";
 import {BitmapManager} from "../resources/BitmapManager";
-import {Bitmaps} from "../resources/Bitmaps";
 import {BitmapUtil} from "../util/BitmapUtil";
 import {Fonts} from "../util/Fonts";
 import {Base} from "./Base";
@@ -12,72 +11,72 @@ import {BaseDrawFlags} from "./BaseDrawFlags";
 export class BaseBitmaps {
     public baseType: number;
 
-    public letter_data: Texture[];  // letters
+    public letterData: Texture[];  // letters
 
-    public body_data: Texture[];     // max-size
-    public f_body_data: Texture[];   // "dontcare"
+    public bodyData: Texture[];     // max-size
+    public fBodyData: Texture[];   // "dontcare"
 
-    public l_body_data: Texture[];   // max-size, letter mode
-    public lf_body_data: Texture[];  // "dontcare"
+    public lBodyData: Texture[];   // max-size, letter mode
+    public lfBodyData: Texture[];  // "dontcare"
 
-    public lock_data: Texture[];     // max-size, locked
-    public f_lock_data: Texture[];   // "dontcare"
+    public lockData: Texture[];     // max-size, locked
+    public fLockData: Texture[];   // "dontcare"
 
-    public mid_data: Texture[];      // mid-size
-    public f_mid_data: Texture[];    // "dontcare"
+    public midData: Texture[];      // mid-size
+    public fMidData: Texture[];    // "dontcare"
 
-    public mid_lock_data: Texture[]; // mid-size, locked
-    public f_mid_lock_data: Texture[]; // "dontcare"
+    public midLockData: Texture[]; // mid-size, locked
+    public fMidLockData: Texture[]; // "dontcare"
 
-    public min_data: Texture; // min-size
+    public minData: Texture; // min-size
 
     constructor (baseType: number) {
         this.baseType = baseType;
-        this.letter_data = BaseBitmaps.createLetterBitmaps(baseType, Base.ZOOM_SCALE_FACTOR);
+        this.letterData = BaseBitmaps.createLetterBitmaps(baseType, Base.ZOOM_SCALE_FACTOR);
 
-        this.body_data = BaseBitmaps.createBodyBitmaps("LBase*", baseType);
-        this.f_body_data = BaseBitmaps.createBodyBitmaps("LBase*f", baseType);
-        this.l_body_data = BaseBitmaps.createBodyBitmaps("LBase*", baseType);
-        this.lf_body_data = BaseBitmaps.createBodyBitmaps("LBase*f", baseType);
-        this.lock_data = BaseBitmaps.createBodyBitmaps("Base*Lock", baseType);
-        this.f_lock_data = BaseBitmaps.createBodyBitmaps("Base*fLock", baseType);
-        this.mid_data = BaseBitmaps.createBodyBitmaps("Base*Mid", baseType);
-        this.f_mid_data = BaseBitmaps.createBodyBitmaps("Base*fMid", baseType);
-        this.mid_lock_data = BaseBitmaps.createBodyBitmaps("Base*MidLock", baseType);
-        this.f_mid_lock_data = BaseBitmaps.createBodyBitmaps("Base*fMidLock", baseType);
-        this.min_data = BitmapManager.get_bitmap_named(BaseBitmaps.getBitmapName("Base*Min", baseType));
+        this.bodyData = BaseBitmaps.createBodyBitmaps("LBase*", baseType);
+        this.fBodyData = BaseBitmaps.createBodyBitmaps("LBase*f", baseType);
+        this.lBodyData = BaseBitmaps.createBodyBitmaps("LBase*", baseType);
+        this.lfBodyData = BaseBitmaps.createBodyBitmaps("LBase*f", baseType);
+        this.lockData = BaseBitmaps.createBodyBitmaps("Base*Lock", baseType);
+        this.fLockData = BaseBitmaps.createBodyBitmaps("Base*fLock", baseType);
+        this.midData = BaseBitmaps.createBodyBitmaps("Base*Mid", baseType);
+        this.fMidData = BaseBitmaps.createBodyBitmaps("Base*fMid", baseType);
+        this.midLockData = BaseBitmaps.createBodyBitmaps("Base*MidLock", baseType);
+        this.fMidLockData = BaseBitmaps.createBodyBitmaps("Base*fMidLock", baseType);
+        this.minData = BitmapManager.get_bitmap_named(BaseBitmaps.getBitmapName("Base*Min", baseType));
     }
 
-    public getBodyBitmap (zoom_level: number, flags: number): Texture {
+    public getBodyBitmap (zoomLevel: number, flags: number): Texture {
         const locked: boolean = (flags & BaseDrawFlags.LOCKED) !== 0;
         const lettermode: boolean = (flags & BaseDrawFlags.LETTER_MODE) !== 0;
         const is_dontcare: boolean = (flags & BaseDrawFlags.IS_DONTCARE) !== 0;
 
-        if (zoom_level < Base.NUM_ZOOM_LEVELS) {
+        if (zoomLevel < Base.NUM_ZOOM_LEVELS) {
             if (!locked && !lettermode) {
-                return BaseBitmaps.bitmap_for_size(is_dontcare ? this.f_body_data : this.body_data, 0, zoom_level);
+                return BaseBitmaps.bitmapForSize(is_dontcare ? this.fBodyData : this.bodyData, 0, zoomLevel);
             } else if (!locked) {
-                return BaseBitmaps.bitmap_for_size(is_dontcare ? this.lf_body_data : this.l_body_data, 0, zoom_level);
+                return BaseBitmaps.bitmapForSize(is_dontcare ? this.lfBodyData : this.lBodyData, 0, zoomLevel);
             } else {
-                return BaseBitmaps.bitmap_for_size(is_dontcare ? this.f_lock_data : this.lock_data, 0, zoom_level);
+                return BaseBitmaps.bitmapForSize(is_dontcare ? this.fLockData : this.lockData, 0, zoomLevel);
             }
-        } else if (zoom_level < Base.NUM_ZOOM_LEVELS * 2) {
+        } else if (zoomLevel < Base.NUM_ZOOM_LEVELS * 2) {
             if (!locked) {
-                return BaseBitmaps.bitmap_for_size(is_dontcare ? this.f_mid_data : this.mid_data, 0, zoom_level - Base.NUM_ZOOM_LEVELS);
+                return BaseBitmaps.bitmapForSize(is_dontcare ? this.fMidData : this.midData, 0, zoomLevel - Base.NUM_ZOOM_LEVELS);
             } else {
-                return BaseBitmaps.bitmap_for_size(is_dontcare ? this.f_mid_lock_data : this.mid_lock_data, 0, zoom_level - Base.NUM_ZOOM_LEVELS);
+                return BaseBitmaps.bitmapForSize(is_dontcare ? this.fMidLockData : this.midLockData, 0, zoomLevel - Base.NUM_ZOOM_LEVELS);
             }
         } else {
-            return this.min_data;
+            return this.minData;
         }
     }
 
-    public getLetterBitmap (zoom_level: number, drawFlags: number): Texture {
+    public getLetterBitmap (zoomLevel: number, drawFlags: number): Texture {
         const lettermode: boolean = (drawFlags & BaseDrawFlags.LETTER_MODE) !== 0;
         const locked: boolean = (drawFlags & BaseDrawFlags.LOCKED) !== 0;
 
-        if (zoom_level < Base.NUM_ZOOM_LEVELS && ((lettermode && !locked))) {
-            return BaseBitmaps.bitmap_for_size(this.letter_data, 0, zoom_level);
+        if (zoomLevel < Base.NUM_ZOOM_LEVELS && ((lettermode && !locked))) {
+            return BaseBitmaps.bitmapForSize(this.letterData, 0, zoomLevel);
         }
 
         return null;
@@ -116,12 +115,12 @@ export class BaseBitmaps {
         }
     }
 
-    private static bitmap_for_size (bitmaps: Texture[], ii: number, size_num: number): Texture {
+    private static bitmapForSize (bitmaps: Texture[], ii: number, size_num: number): Texture {
         if (bitmaps.length % Base.NUM_ZOOM_LEVELS !== 0) {
             throw new Error("Invalid bitmaps array length " + bitmaps.length);
         }
 
-        let orig_length: number = bitmaps.length / Base.NUM_ZOOM_LEVELS;
-        return bitmaps[(orig_length * size_num + ii)];
+        let origLength: number = bitmaps.length / Base.NUM_ZOOM_LEVELS;
+        return bitmaps[(origLength * size_num + ii)];
     }
 }
