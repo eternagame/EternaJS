d761a7564fd0486b61b6c8fa64597db6779090ea
Tim Conkling <tconkling@gmail.com>
2018-08-28 12:50:11 -0700

    RankBoard: camelCase

--- src/eterna/rank/RankBoard.ts
+++ src/eterna/rank/RankBoard.ts
@@ -5,23 +5,23 @@ import {PlayerRank} from "./PlayerRank";
 import {RankRowLayout} from "./RankRowLayout";
 
 export class RankBoard extends ContainerObject implements Updatable {
-    public static readonly ROW_WIDTH: number = 400;
-    public static readonly ROW_HEIGHT: number = 20;
-    public static readonly PLAYER_ROW_HEIGHT: number = 32;
+    public static readonly ROW_WIDTH = 400;
+    public static readonly ROW_HEIGHT = 20;
+    public static readonly PLAYER_ROW_HEIGHT = 32;
 
-    public constructor(starting_rank: number, rank_data: PlayerRank[], offset_btw_rank_coin: number) {
+    public constructor(startingRank: number, rankData: PlayerRank[], offsetBtwRankCoin: number) {
         super();
-        this._rankData = rank_data;
-        this._starting_position = 0;
+        this._rankData = rankData;
+        this._startingPosition = 0;
 
-        let num_rows: number = RankBoard.NUM_VISIBLE;
-        if (rank_data.length < RankBoard.NUM_VISIBLE + 1) {
-            num_rows = rank_data.length - 1;
+        let numRows = RankBoard.NUM_VISIBLE;
+        if (rankData.length < RankBoard.NUM_VISIBLE + 1) {
+            numRows = rankData.length - 1;
         }
 
         this._rows = [];
-        for (let ii: number = 0; ii < num_rows + 1; ii++) {
-            let row: RankRowLayout = new RankRowLayout(ii + starting_rank, rank_data[ii], offset_btw_rank_coin, 15, 100);
+        for (let ii = 0; ii < numRows + 1; ii++) {
+            let row: RankRowLayout = new RankRowLayout(ii + startingRank, rankData[ii], offsetBtwRankCoin, 15, 100);
             row.container.height = RankBoard.ROW_HEIGHT;
             row.display.position = new Point(0, RankBoard.ROW_HEIGHT * ii);
             this.addObject(row, this.container);
@@ -30,12 +30,12 @@ export class RankBoard extends ContainerObject implements Updatable {
     }
 
     // _starting_position MUST be set prior to animation or the animation will fly all over the place.
-    public register_starting_pos(): void {
-        this._starting_position = this.container.y;
+    public registerStartingPos(): void {
+        this._startingPosition = this.container.y;
     }
 
     public update(dt: number): void {
-        let parentOffset: number = this.container.y - this._starting_position;
+        let parentOffset: number = this.container.y - this._startingPosition;
         for (let ii: number = 0; ii < this._rows.length; ii++) {
             // pos_offset / RankBoard.ROW_HEIGHT = How many entries we have moved by
             let loopNum: number = Math.floor((parentOffset / RankBoard.ROW_HEIGHT - ii + 2) / 3); // How many times a row has looped
@@ -57,7 +57,7 @@ export class RankBoard extends ContainerObject implements Updatable {
     private readonly _rankData: PlayerRank[];
     private readonly _rows: RankRowLayout[];
 
-    private _starting_position: number;
+    private _startingPosition: number;
 
     private static readonly NUM_VISIBLE: number = 2; // the number of entries you want visible at once (intermediary entry is automatically added)
 }
