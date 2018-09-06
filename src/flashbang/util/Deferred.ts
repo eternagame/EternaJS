07fadcea0d39fad90294542d70045c1fa18f051d
Tim Conkling <tconkling@gmail.com>
2018-09-06 10:14:08 -0700

    ExternalInterface: use a nicer wrapper for our deferred promise

---
+++ src/flashbang/util/Deferred.ts
@@ -0,0 +1,41 @@
+/** Wraps a Promise and allows it to be resolved or rejected later. */
+export class Deferred<T> {
+    public readonly promise: Promise<T>;
+
+    public constructor() {
+        this.promise = new Promise((resolve, reject) => {
+            this._resolve = resolve;
+            this._reject = reject;
+        });
+    }
+
+    /** True if the promise has been resolved or rejected */
+    public get isSealed(): boolean {
+        return this._resolve == null;
+    }
+
+    public resolve(value?: PromiseLike<T> | T): void {
+        if (this.isSealed) {
+            throw new Error("Can't resolve sealed promise");
+        }
+
+        let fn = this._resolve;
+        this._resolve = null;
+        this._reject = null;
+        fn(value);
+    }
+
+    public reject(reason?: any): void {
+        if (this.isSealed) {
+            throw new Error("Can't reject sealed promise");
+        }
+
+        let fn = this._reject;
+        this._resolve = null;
+        this._reject = null;
+        fn(reason);
+    }
+
+    private _resolve: (value?: PromiseLike<T> | T) => void;
+    private _reject: (reason?: any) => void;
+}
