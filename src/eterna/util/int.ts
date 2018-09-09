acef072309a6baed670c46a82b4e1a083da8f04d
Tim Conkling <tconkling@gmail.com>
2018-09-09 16:03:43 -0700

    "malus" is passed to cofold_sequence as an int in flash; do the same here

---
+++ src/eterna/util/int.ts
@@ -0,0 +1,4 @@
+/** Emulates the int() function in AS3: casts the given value to a number and floors it */
+export function int(value: string | number): number {
+    return Math.floor(Number(value));
+}
