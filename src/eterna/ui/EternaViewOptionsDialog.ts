af06c2037978e2c89a92f1d6d0f4501fe224b5d9
Tim Conkling <tconkling@gmail.com>
2018-09-11 17:44:04 -0700

    view options dialog: sound volume & mute controls

--- src/eterna/ui/EternaViewOptionsDialog.ts
+++ src/eterna/ui/EternaViewOptionsDialog.ts
@@ -1,8 +1,11 @@
+import {Point} from "pixi.js";
 import {HAlign, VAlign} from "../../flashbang/core/Align";
+import {HLayoutContainer} from "../../flashbang/layout/HLayoutContainer";
 import {VLayoutContainer} from "../../flashbang/layout/VLayoutContainer";
 import {Setting} from "../../flashbang/settings/Setting";
 import {DisplayUtil} from "../../flashbang/util/DisplayUtil";
 import {Eterna} from "../Eterna";
+import {Bitmaps} from "../resources/Bitmaps";
 import {Dialog} from "./Dialog";
 import {GameButton} from "./GameButton";
 import {GameCheckbox} from "./GameCheckbox";
@@ -48,16 +51,48 @@ export class EternaViewOptionsDialog extends Dialog<void> {
             bind(Eterna.settings.displayAuxInfo, "Display auxiliary information about RNAs");
         }
 
-        let viewLayout: VLayoutContainer = new VLayoutContainer(22, HAlign.CENTER);
+        const NUM_VOLUME_BUTTONS = 5;
+
+        let soundButtonLayout = new HLayoutContainer(4);
+        settingsLayout.addChild(soundButtonLayout);
+
+        this._muteButton = new GameButton().allStates(Bitmaps.AudioNormal);
+        this._muteButton.display.scale = new Point(0.7, 0.7);
+        // Don't play the default button sound when the button is clicked -
+        // we want to update the volume first, and *then* play the sound.
+        this._muteButton.downSound = null;
+        this._muteButton.clicked.connect(() => {
+            this.setVolume(!Eterna.settings.soundMute.value, Eterna.settings.soundVolume.value);
+            // Play the button-clicked sound after the volume has been adjusted.
+            Eterna.sound.playSound(GameButton.DEFAULT_DOWN_SOUND);
+        });
+        this.addObject(this._muteButton, soundButtonLayout);
+
+        for (let ii = 0; ii < NUM_VOLUME_BUTTONS; ++ii) {
+            let volumeButton = new GameButton().allStates(Bitmaps.Audio_Vol_On);
+            volumeButton.display.scale = new Point(0.3, 0.3);
+            volumeButton.downSound = null;
+            volumeButton.clicked.connect(() => {
+                this.setVolume(false, (ii + 1) / NUM_VOLUME_BUTTONS);
+                Eterna.sound.playSound(GameButton.DEFAULT_DOWN_SOUND);
+            });
+            this._volumeButtons.push(volumeButton);
+            this.addObject(volumeButton, soundButtonLayout);
+        }
+
+        // This will update the sound buttons to their proper start states
+        this.setVolume(Eterna.settings.soundMute.value, Eterna.settings.soundVolume.value);
+
+        let viewLayout = new VLayoutContainer(22, HAlign.CENTER);
         viewLayout.addChild(settingsLayout);
 
-        let ok_button: GameButton = new GameButton().label("Done", 14);
-        this.addObject(ok_button, viewLayout);
-        ok_button.clicked.connect(() => this.close(null));
+        let okButton: GameButton = new GameButton().label("Done", 14);
+        this.addObject(okButton, viewLayout);
+        okButton.clicked.connect(() => this.close(null));
 
         viewLayout.layout();
 
-        let panel: GamePanel = new GamePanel(GamePanelType.NORMAL, 1, 0x152843, 0.27, 0xC0DCE7);
+        let panel = new GamePanel(GamePanelType.NORMAL, 1, 0x152843, 0.27, 0xC0DCE7);
         panel.title  = "Game options";
         panel.setSize(viewLayout.width + 40, viewLayout.height + 40 + panel.titleHeight);
         this.addObject(panel, this.container);
@@ -104,8 +139,22 @@ export class EternaViewOptionsDialog extends Dialog<void> {
         return 0.3;
     }
 
+    private setVolume(mute: boolean, volume: number): void {
+        Eterna.settings.soundMute.value = mute;
+        Eterna.settings.soundVolume.value = volume;
+
+        this._muteButton.allStates(mute ? Bitmaps.AudioMute : Bitmaps.AudioNormal);
+
+        let numVolumeButtons = this._volumeButtons.length;
+        for (let ii = 0; ii < numVolumeButtons; ++ii) {
+            let volumeButton = this._volumeButtons[ii];
+            let on = !mute && volume >= (ii + 1) / numVolumeButtons;
+            volumeButton.allStates(on ? Bitmaps.Audio_Vol_On : Bitmaps.Audio_Vol_Off);
+        }
+    }
+
     private static createCheckbox(title: string, setting: Setting<boolean>): GameCheckbox {
-        let checkbox: GameCheckbox = new GameCheckbox(18, title);
+        let checkbox = new GameCheckbox(18, title);
         checkbox.toggled.value = setting.value;
         checkbox.regs.add(setting.connect(checkbox.toggled.slot));
         checkbox.regs.add(checkbox.toggled.connect(setting.slot));
@@ -113,4 +162,6 @@ export class EternaViewOptionsDialog extends Dialog<void> {
     }
 
     private readonly _optionsMode: EternaViewOptionsMode;
+    private _muteButton: GameButton;
+    private _volumeButtons: GameButton[] = [];
 }
