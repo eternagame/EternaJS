849412397e30a63326eb4247590cc0658d14c028
Tim Conkling <tconkling@gmail.com>
2018-09-23 12:30:29 -0700

    Fix GridLines

--- src/eterna/mode/DesignBrowser/GridLines.ts
+++ src/eterna/mode/DesignBrowser/GridLines.ts
@@ -1,26 +1,36 @@
 import {Graphics} from "pixi.js";
-import {int} from "../../util/int";
 
 // GridLines class written by Jerry Fu
-// Draws any number of lines covering the entire width of the design browser at a specified interval, starting from the top heading down
+// Draws any number of lines covering the entire width of the design browser at a specified interval,
+// starting from the top heading down
 export class GridLines extends Graphics {
-    constructor(thickness: number, color: number, interval_spacing: number) {
+    constructor(thickness: number, color: number, intervalSpacing: number) {
         super();
         this._thickness = thickness;
         this._color = color;
-        this._interval_spacing = interval_spacing;
+        this._intervalSpacing = intervalSpacing;
     }
 
-    public redraw(width: number, yPos: number) {
+    public setSize(width: number, height: number): void {
+        if (this._width === width && this._height === height) {
+            return;
+        }
+
+        this._width = width;
+        this._height = height;
         this.clear();
+
         this.lineStyle(this._thickness, this._color);
-        for (let ii = 0; ii < int((width - 32 - yPos) / this._interval_spacing); ii++) {
-            this.moveTo(5, ii * this._interval_spacing);
-            this.lineTo(width - 5, ii * this._interval_spacing);
+        for (let ii = 0; ii < Math.round(height / this._intervalSpacing); ii++) {
+            this.moveTo(5, ii * this._intervalSpacing);
+            this.lineTo(width - 5, ii * this._intervalSpacing);
         }
     }
 
     private readonly _thickness: number;
     private readonly _color: number;
-    private readonly _interval_spacing: number;
+    private readonly _intervalSpacing: number;
+
+    private _width: number = 0;
+    private _height: number = 0;
 }
