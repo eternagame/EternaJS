fbd09e606397934d2e442f2bc55ed5cced3c922f
Tim Conkling <tconkling@gmail.com>
2018-09-26 14:19:58 -0700

    ui.FixedWidthTextField

---
+++ src/eterna/ui/FixedWidthTextField.ts
@@ -0,0 +1,46 @@
+import {Container, Graphics, Text, TextStyle} from "pixi.js";
+import {HAlign} from "../../flashbang/core/Align";
+
+export class FixedWidthTextField extends Container {
+    public constructor(text: string, style: TextStyle, width: number, hAlign: HAlign = HAlign.LEFT) {
+        super();
+
+        this._width = width;
+        this._halign = hAlign;
+        this._tf = new Text("", style);
+        this.addChild(new Graphics().beginFill(0x0, 0).drawRect(0, 0, width, this._tf.height).endFill());
+        this.addChild(this._tf);
+
+        this.text = text;
+    }
+
+    public get text(): string {
+        return this._text;
+    }
+
+    public set text(value: string) {
+        this._text = value;
+        this._tf.text = value;
+        while (this._tf.width > this._width && this._tf.text.length > 0) {
+            this._tf.text = this._tf.text.substr(0, this._tf.text.length - 1);
+        }
+
+        switch (this._halign) {
+        case HAlign.LEFT:
+            this._tf.x = 0;
+            break;
+        case HAlign.CENTER:
+            this._tf.x = (this._width - this._tf.width) * 0.5;
+            break;
+        case HAlign.RIGHT:
+            this._tf.x = this._width - this._tf.width;
+            break;
+        }
+    }
+
+    private readonly _width: number;
+    private readonly _tf: Text;
+    private readonly _halign: HAlign;
+
+    private _text: string;
+}
