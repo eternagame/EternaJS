baeeb56fb57c4a32d7456be4ac5afbc573a8a7c5
LFP6 <LFP6.01@gmail.com>
2018-12-19 01:52:27 -0500

    Don't panic since we apparently call a TextInputObject text set from within a valueChanged or something

--- src/eterna/ui/TextInputObject.ts
+++ src/eterna/ui/TextInputObject.ts
@@ -146,7 +146,6 @@ export class TextInputObject extends DOMObject<HTMLInputElement | HTMLTextAreaEl
 
     public set text(value: string) {
         this._obj.value = value;
-        this.valueChanged.emit(value);
     }
 
     public get caretPosition(): number {
