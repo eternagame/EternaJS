2f5af4e155dc90a6fa42cbf3d27a52329fc90bd6
LFP6 <LFP6.01@gmail.com>
2018-12-21 23:56:02 -0600

    Upadte CMake and bindings for new LinearFold version

--- lib/LinearFold/emscripten/FullFold.cpp
+++ lib/LinearFold/emscripten/FullFold.cpp
@@ -1,14 +1,11 @@
 #include "FullFold.h"
-#include "beamckypar.h"
+#include "LinearFold.h"
 
 FullFoldResult* FullFoldDefault (std::string seqString) {
     int beamsize = 100;
-    bool use_vienna = false;
-    bool is_cube_pruning = true;
-    bool is_candidate_list = true;
     bool sharpturn = false;
 
-    BeamCKYParser parser(beamsize, use_vienna, is_candidate_list, !sharpturn, is_cube_pruning);
+    BeamCKYParser parser(beamsize, !sharpturn);
     BeamCKYParser::DecoderResult decoderResult = parser.parse(seqString);
 
     FullFoldResult* result = new FullFoldResult();
