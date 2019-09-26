8f7452e1d12b500a4e0770e6dbc628cfd319ba85
Rhiju Das <rhiju@stanford.edu>
2019-09-26 08:46:43 -0700

    correct baseRope lateUpdate during screen resize. revert cubic-spline NPM package back to lighter-weight function for speed.

--- src/eterna/pose2D/BaseRope.ts
+++ src/eterna/pose2D/BaseRope.ts
@@ -2,7 +2,6 @@ import { DisplayObject, Graphics, Point } from "pixi.js";
 import { GameObject } from "../../flashbang/core/GameObject";
 import { LateUpdatable } from "../../flashbang/core/LateUpdatable";
 import { Pose2D } from "./Pose2D";
-import * as Spline from "cubic-spline"
 import { Vector2 } from "../../flashbang/geom/Vector2";
 
 /** BaseRope: A class for drawing a smooth 'rope' through bases. **/
@@ -51,9 +50,6 @@ export class BaseRope extends GameObject implements LateUpdatable {
             }
           }
 
-        const splineX = new Spline(idx, baseposX);
-        const splineY = new Spline(idx, baseposY);
-
         // by drawing twice, can get a nice looking texture.
         // draw thick line and thin line on top
         let OUTER_ROPE_THICKNESS : number = 0.30 * Pose2D.ZOOM_SPACINGS[this._pose._zoomLevel];
@@ -61,23 +57,57 @@ export class BaseRope extends GameObject implements LateUpdatable {
 
         this._graphics.lineStyle(OUTER_ROPE_THICKNESS, 0x777777, 0.2);
         this._graphics.moveTo(baseposX[0], baseposY[0]);
-        this.drawBaseRopeLine(splineX, splineY);
+        // this.drawBaseRopeLine(splineX, splineY);
+        this.drawBaseRopeLine(baseposX, baseposY);
 
         this._graphics.lineStyle(INNER_ROPE_THICKNESS, 0xE8E8E8, 0.2);
         this._graphics.moveTo(baseposX[0], baseposY[0]);
-        this.drawBaseRopeLine(splineX, splineY);
+        // this.drawBaseRopeLine(splineX, splineY);
+        this.drawBaseRopeLine(baseposX, baseposY);
     }
 
-    private drawBaseRopeLine(splineX: Spline, splineY: Spline): void {
-        let smooth_factor: integer = 3;
+    private drawBaseRopeLine(baseposX: number[], baseposY: number[]): void {
+        // by drawing twice, can get a nice looking texture.
+        this._graphics.moveTo(baseposX[0], baseposY[0]);
+        // Update the points to correspond with base locations
+        // Note that this could be way smarter -- just look at rope segments that need to be updated, not entire rope.
+        let smooth_factor: integer = 5;
         for (let i = 1; i < this._pose.fullSequence.length * smooth_factor; i++) {
             // Smooth the curve with cubic interpolation to prevent sharp edges.
-            const ix = splineX.at(i / smooth_factor);
-            const iy = splineY.at(i / smooth_factor);
+            const ix = this.cubicInterpolation(baseposX, i / smooth_factor);
+            const iy = this.cubicInterpolation(baseposY, i / smooth_factor);
             this._graphics.lineTo(ix, iy);
         }
     }
 
+    // adapted directly from  demo    
+    //         https://pixijs.io/examples/#/demos-advanced/mouse-trail.js
+    //  Cubic interpolation based on https://github.com/osuushi/Smooth.js
+    //
+    // Note: This is way faster than cubic-interpolation.js available through NPM.
+    //
+    private cubicInterpolation(array, t, tangentFactor) {
+        if (tangentFactor == null) tangentFactor = 1;
+
+        const k = Math.floor(t);
+        const m = [this.getTangent(k, tangentFactor, array), this.getTangent(k + 1, tangentFactor, array)];
+        const p = [this.clipInput(k, array), this.clipInput(k + 1, array)];
+        t -= k;
+        const t2 = t * t;
+        const t3 = t * t2;
+        return (2 * t3 - 3 * t2 + 1) * p[0] + (t3 - 2 * t2 + t) * m[0] + (-2 * t3 + 3 * t2) * p[1] + (t3 - t2) * m[1];
+    }
+
+    private getTangent(k, factor, array) {
+        return factor * (this.clipInput(k + 1, array) - this.clipInput(k - 1, array)) / 2;
+    }
+    private clipInput(k, arr) {
+        if (k < 0) k = 0;
+        if (k > arr.length - 1) k = arr.length - 1;
+        return arr[k];
+    }
+
+    // following not in use -- delete if still not in use in 2020.
     public makeVisibleIfLongSpacings(): void {
         if (this._visible) return;
         for (let i = 1; i < this._pose.fullSequence.length; i++) {
