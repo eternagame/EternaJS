4fe324c255fb94ceeceb54b5b1559542fc6c97a7
Rhiju Das <rhiju@stanford.edu>
2019-10-22 15:50:56 -0700

    lint fixes to triangulate.ts

--- src/eterna/pose2D/triangulate.ts
+++ src/eterna/pose2D/triangulate.ts
@@ -3,38 +3,37 @@ import libtess from 'libtess';
 // libtess. Also tried PolyK, poly2tri, and earcut, and they failed for scoreNode -- rhiju.
 // see https://brendankenny.github.io/libtess.js/examples/simple_triangulation/triangulate.js
 
-    // function called for each vertex of tesselator output
-    function vertexCallback(data: number[], polyVertArray: number[]) {
-        polyVertArray[polyVertArray.length] = data[0];
-        polyVertArray[polyVertArray.length] = data[1];
-    }
-    // function begincallback(type) {
-    //     if (type !== libtess.primitiveType.GL_TRIANGLES) {
-    //         // console.log(`expected TRIANGLES but got type: ${type}`);
-    //     }
-    // }
-    // function errorcallback(errno) {
-    //     // console.log('error callback');
-    //     // console.log(`error number: ${errno}`);
-    // }
-    // callback for when segments intersect and must be split
-    function combinecallback(coords: number[], data: number[], weight: number) {
-        // console.log('combine callback');
-        return [coords[0], coords[1], coords[2]];
-    }
-    // function edgeCallback(flag) {
-    //     // don't really care about the flag, but need no-strip/no-fan behavior
-    //     // console.log('edge flag: ' + flag);
-    // }
+// function called for each vertex of tesselator output
+function vertexCallback(data: number[], polyVertArray: number[]) {
+    polyVertArray[polyVertArray.length] = data[0];
+    polyVertArray[polyVertArray.length] = data[1];
+}
+// function begincallback(type) {
+//     if (type !== libtess.primitiveType.GL_TRIANGLES) {
+//         // console.log(`expected TRIANGLES but got type: ${type}`);
+//     }
+// }
+// function errorcallback(errno) {
+//     // console.log('error callback');
+//     // console.log(`error number: ${errno}`);
+// }
+// callback for when segments intersect and must be split
+function combinecallback(coords: number[], data: number[], weight: number) {
+    // console.log('combine callback');
+    return [coords[0], coords[1], coords[2]];
+}
+// function edgeCallback(flag) {
+//     // don't really care about the flag, but need no-strip/no-fan behavior
+//     // console.log('edge flag: ' + flag);
+// }
 
-    const tessy = new libtess.GluTesselator();
-    // tessy.gluTessProperty(libtess.gluEnum.GLU_TESS_WINDING_RULE, libtess.windingRule.GLU_TESS_WINDING_POSITIVE);
-    tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_VERTEX_DATA, vertexCallback);
-    // tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_BEGIN, begincallback);
-    // tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_ERROR, errorcallback);
-    tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_COMBINE, combinecallback);
-    // tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_EDGE_FLAG, edgeCallback);
-
+const tessy = new libtess.GluTesselator();
+// tessy.gluTessProperty(libtess.gluEnum.GLU_TESS_WINDING_RULE, libtess.windingRule.GLU_TESS_WINDING_POSITIVE);
+tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_VERTEX_DATA, vertexCallback);
+// tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_BEGIN, begincallback);
+// tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_ERROR, errorcallback);
+tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_COMBINE, combinecallback);
+// tessy.gluTessCallback(libtess.gluEnum.GLU_TESS_EDGE_FLAG, edgeCallback);
 
 
 export default function triangulate(contour: number[]): number[] {
