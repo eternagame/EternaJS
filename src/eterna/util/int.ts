15b32a32b2d309e1a091c0521e2e77bc5dd0a721
Andy Watkins <andy.watkins2@gmail.com>
2019-11-02 16:23:35 -0700

    Tests for int

--- src/eterna/util/int.ts
+++ src/eterna/util/int.ts
@@ -1,4 +1,9 @@
-/** Emulates the int() function in AS3: casts the given value to a number and floors it */
+/**
+ *
+ * Emulates the int() function in AS3: casts the given value to a number and floors it
+ *
+ * @param value either a string or number to be rounded.
+ */
 export default function int(value: string | number): number {
     return Math.floor(Number(value));
 }
