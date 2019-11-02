27eebfc6e5d08bde83fef12d0f0ff901886b6298
Andy Watkins <andy.watkins2@gmail.com>
2019-11-02 16:40:14 -0700

    Fix tsdoc

--- src/eterna/util/int.ts
+++ src/eterna/util/int.ts
@@ -2,7 +2,9 @@
  *
  * Emulates the int() function in AS3: casts the given value to a number and floors it
  *
- * @param value either a string or number to be rounded.
+ * @param value either a string or number to be floored.
+ *
+ * @returns a number, the floored value
  */
 export default function int(value: string | number): number {
     return Math.floor(Number(value));
