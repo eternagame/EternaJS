6135807cee786295991d6db153d2a29431208a1d
Andy Watkins <andy.watkins2@gmail.com>
2019-11-11 19:00:13 -0800

    Get rid of old vienna

--- lib/vienna/cofold.h
+++
@@ -1,9 +0,0 @@
-/* function from fold.c */
-#include "subopt.h"
-extern float  cofold(char *sequence, char *structure); 
-extern float  cofold_with_binding_site(const char *sequence, char *structure, int i, int p, int j, int q, int bonus); // NNFIX
-/* calculate energy of string on structure */
-extern void   free_co_arrays(void);          /* free arrays for mfe folding */
-extern void   initialize_cofold(int length); /* allocate arrays for folding */
-extern void   update_cofold_params(void);    /* recalculate parameters */
-extern SOLUTION *zukersubopt(const char *string);
--- lib/vienna/ribo.h
+++
@@ -1,1 +0,0 @@
-extern float **get_ribosum(char *Alseq[], int n_seq, int length);
