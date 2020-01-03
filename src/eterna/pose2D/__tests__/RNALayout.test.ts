cd317e9ead151de812227289ddfb9983ba96ee9a
Andy Watkins <andy.watkins2@gmail.com>
2020-01-03 16:36:36 -0500

    Remvoe hooks for mocking that were commented out

--- src/eterna/pose2D/__tests__/RNALayout.test.ts
+++ src/eterna/pose2D/__tests__/RNALayout.test.ts
@@ -7,7 +7,4 @@ test(`RNALayout:setupTree`, () => {
     let pairs: number[] = [10, 9, 8, -1, -1, -1, -1, 3, 2, 1];
     rnalayout.setupTree(pairs);
     expect(rnalayout["_scoreBiPairs"][0]).toBe(11);
-    //expect(RNALayout.mock["addNodesRecursive"].calls.length).toBe(3);
-    // should call addNodesRecursive three times, or maybe six
-    
 });
