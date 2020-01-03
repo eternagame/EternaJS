6debe43a293034778fdeb8d5668d6d2320a93454
Andy Watkins <andy.watkins2@gmail.com>
2020-01-03 16:27:59 -0500

    Don't annotate a Promise<void> return

--- src/eterna/net/GameClient.ts
+++ src/eterna/net/GameClient.ts
@@ -56,8 +56,11 @@ export default class GameClient {
 
     /**
      * Logs the player out.
+<<<<<<< Updated upstream
      *
      * @returns resolves to void
+=======
+>>>>>>> Stashed changes
      */
     public logout(): Promise<void> {
         return this.get('/eterna_logout.php', {noredirect: true})
