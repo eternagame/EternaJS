8321a66c5f3abf60b9b0bf4e467cf45a7a27b932
Andy Watkins <andy.watkins2@gmail.com>
2020-04-13 15:54:27 -0400

    BaseShapeConstraint getHighlight thus also returns HighlightInfo | null (even though I don't think this one can give null! )

--- src/eterna/constraints/constraints/ShapeConstraint.ts
+++ src/eterna/constraints/constraints/ShapeConstraint.ts
@@ -90,7 +90,7 @@ abstract class BaseShapeConstraint extends Constraint<ShapeConstraintStatus> {
         };
     }
 
-    public getHighlight(status: ShapeConstraintStatus): HighlightInfo {
+    public getHighlight(status: ShapeConstraintStatus): HighlightInfo | null {
         let unstable: number[] = [];
         if (status.wrongPairs) {
             let curr = 0;
