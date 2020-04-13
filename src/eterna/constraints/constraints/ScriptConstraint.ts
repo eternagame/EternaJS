810775863846e7574f1fd5723cbfce0aa3e5dc62
Andy Watkins <andy.watkins2@gmail.com>
2020-04-13 16:00:19 -0400

    ensure _statText is only compared to undefined -- never null

--- src/eterna/constraints/constraints/ScriptConstraint.ts
+++ src/eterna/constraints/constraints/ScriptConstraint.ts
@@ -46,7 +46,7 @@ export default class ScriptConstraint extends Constraint<ScriptConstraintStatus>
             showOutline: true,
             icon: status.dataPNG,
             stateNumber: status.stateIndex + 1,
-            statText: !forMissionScreen ? status.resultValue : null,
+            statText: !forMissionScreen ? status.resultValue : undefined,
             tooltip: ConstraintBox.createTextStyle().append(status.goal || `Your puzzle must satisfy script ${this.scriptID}`)
         };
     }
