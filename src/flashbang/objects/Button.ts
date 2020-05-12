3254c28ce6e51cc35241193444b7fb6ebefc65df
Amine Rehioui <amine@franticsoftware.com>
2020-05-12 19:00:05 -0400

    Prevent click event if button is disabled

--- src/flashbang/objects/Button.ts
+++ src/flashbang/objects/Button.ts
@@ -41,7 +41,9 @@ export default abstract class Button extends ContainerObject implements Enableab
         this.regs.add(this.pointerDown.filter(InputUtil.IsLeftMouse).connect(() => this.onPointerDown()));
         this.regs.add(this.pointerUp.filter(InputUtil.IsLeftMouse).connect(() => this.onPointerUp(true)));
         this.regs.add(this.pointerTap.filter(InputUtil.IsLeftMouse).connect(() => {
-            this.clicked.emit();
+            if (this.enabled) {
+                this.clicked.emit();
+            }
         }));
     }
 
