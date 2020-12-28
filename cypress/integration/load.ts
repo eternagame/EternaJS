90e9d1bd0fc37eb410bbbd87bafa9c38b65bbcbc
Andy Watkins <andy.watkins2@gmail.com>
2020-12-28 13:28:21 -0800

    Adding in all the tutorial levels

--- cypress/integration/load.ts
+++ cypress/integration/load.ts
@@ -4,8 +4,58 @@ describe('load representative puzzles', () => {
     //    // cy.visit('https://eternagame.org');
     //    expect(true).to.equal(true);
     //});
-    it('does not crash on 3263276', () => {
-        cy.visit('http://localhost:63343?puzzle=3263276');
+    it('solves tutorial 1.1', () => {
+        cy.visit('http://localhost:63343?puzzle=6502927');
+        // cy.visit('https://eternagame.org');
+        expect(true).to.equal(true);
+    });
+    it('does not crash on 1.2', () => {
+        cy.visit('http://localhost:63343?puzzle=6502942');
+        // cy.visit('https://eternagame.org');
+        expect(true).to.equal(true);
+    });
+    it('does not crash on 1.3', () => {
+        cy.visit('http://localhost:63343?puzzle=6502943');
+        // cy.visit('https://eternagame.org');
+        expect(true).to.equal(true);
+    });
+    it('does not crash on 1.4', () => {
+        cy.visit('http://localhost:63343?puzzle=6502944');
+        // cy.visit('https://eternagame.org');
+        expect(true).to.equal(true);
+    });
+    it('does not crash on 1.5', () => {
+        cy.visit('http://localhost:63343?puzzle=6785384');
+        // cy.visit('https://eternagame.org');
+        expect(true).to.equal(true);
+    });
+    it('does not crash on 1.6', () => {
+        cy.visit('http://localhost:63343?puzzle=6502945');
+        // cy.visit('https://eternagame.org');
+        expect(true).to.equal(true);
+    });
+    it('does not crash on 1.7', () => {
+        cy.visit('http://localhost:63343?puzzle=6502946');
+        // cy.visit('https://eternagame.org');
+        expect(true).to.equal(true);
+    });
+    it('does not crash on 1.8', () => {
+        cy.visit('http://localhost:63343?puzzle=6502947');
+        // cy.visit('https://eternagame.org');
+        expect(true).to.equal(true);
+    });
+    it('does not crash on 1.9', () => {
+        cy.visit('http://localhost:63343?puzzle=6502948');
+        // cy.visit('https://eternagame.org');
+        expect(true).to.equal(true);
+    });
+    it('does not crash on 1.10', () => {
+        cy.visit('http://localhost:63343?puzzle=6502949');
+        // cy.visit('https://eternagame.org');
+        expect(true).to.equal(true);
+    });
+    it('does not crash on 1.11', () => {
+        cy.visit('http://localhost:63343?puzzle=6502950');
         // cy.visit('https://eternagame.org');
         expect(true).to.equal(true);
     });
