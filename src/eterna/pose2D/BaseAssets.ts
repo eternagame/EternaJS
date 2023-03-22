ca7d87453563bad0f2873e249944b921eb241c5d
Jonathan Romano <jonathan@luxaritas.com>
2023-03-22 17:26:17 -0400

    Tweak coloring

--- src/eterna/pose2D/BaseAssets.ts
+++ src/eterna/pose2D/BaseAssets.ts
@@ -509,6 +509,7 @@ export default class BaseAssets {
         */
 
         // lock.filters = [new BlurFilter(1, 40), new AdjustmentFilter({alpha: 0.85})];
+        lock.filters = [new AdjustmentFilter({alpha: 0.85})];
         lockWrapper.addChild(lock);
 
         const lockData = [EternaTextureUtil.scaleBy(
