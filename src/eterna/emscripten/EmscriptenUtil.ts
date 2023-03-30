cdbdd1d0ec87b141ff2d68dc2a774d287911abf8
Jonathan Romano <jonathan@luxaritas.com>
2023-03-30 13:13:52 -0400

    Update WASM components to compile + load under newer emscripten versions (#686)
    ## Summary
    This includes a handful of changes to account for recent(ish) changes in
    Emscripten that caused issues when compiling with newer versions
    
    ## Implementation Notes
    * In 3.1.27 (released 11/29/22), the default stack size was reduced from
    5MB to 64KB. We need larger available stack space, so we revert this
    setting
    * In v1.38.41 (released 08/07/2019), callMain was no longer exported by
    default. Only nupack exports a main function we intend to call, so we
    enabled it specifically for that module
    * `-Wimplicit-int` and `-Wint-conversion` were enabled, but cause errors
    in vendor Vienna code. We revert those errors to warnings.
    * In 1.39.16 (released 05/15/2020), Emscripten modules now provide an
    actual promise instead of a "promise-like" interface, which we now use
    
    ## Testing
    Basic sanity checks loading puzzles, unit tests

--- src/eterna/emscripten/EmscriptenUtil.ts
+++ src/eterna/emscripten/EmscriptenUtil.ts
@@ -7,25 +7,12 @@ export default class EmscriptenUtil {
      * Instantiates a program from an Emscripten module and calls its main() function if it has one.
      * @returns {Promise<any>} a promise that will resolve with the instantiated module.
      */
-    public static loadProgram(module: any): Promise<any> {
-        return new Promise<any>((resolve, _) => {
-            module.default({noInitialRun: true}).then((program: any) => {
-                if (Object.prototype.hasOwnProperty.call(program, 'callMain')) {
-                    program.callMain();
-                }
-
-                // Fix an infinite loop.
-                // "program" is not a promise, but since it has a 'then' property,
-                // resolving our promise with the program will recurse infinitely.
-                // https://github.com/kripken/emscripten/issues/5820
-                // TODO: remove this if and when emscripten is fixed
-                if (Object.prototype.hasOwnProperty.call(program, 'then')) {
-                    delete program['then'];
-                }
-
-                resolve(program);
-            });
-        });
+    public static async loadProgram(module: any): Promise<any> {
+        const program = await module.default({noInitialRun: true});
+        if (Object.prototype.hasOwnProperty.call(program, 'callMain')) {
+            program.callMain();
+        }
+        return program;
     }
     /* eslint-enable @typescript-eslint/no-explicit-any */
 
