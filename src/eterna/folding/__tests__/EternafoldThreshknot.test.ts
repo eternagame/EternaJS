2eac08d28199c8d6401729489e7c1f4e8eef21da
Thomas Karagianes <tkaragianes@stanford.edu>
2023-04-05 13:43:05 -0700

    Update EternaFoldThreshknot tests
    Add new tests to confirm that EternaFoldThreshknot is correctly
    falling back to EternaFold when pseudoknotted is false, and that
    EternaFoldThreshknot is correctly removing single pair helices.

--- src/eterna/folding/__tests__/EternafoldThreshknot.test.ts
+++ src/eterna/folding/__tests__/EternafoldThreshknot.test.ts
@@ -3,6 +3,7 @@ import Folder from '../Folder';
 import SecStruct from 'eterna/rnatypes/SecStruct';
 import Sequence from 'eterna/rnatypes/Sequence';
 import EternaFoldThreshknot from 'eterna/folding/EternafoldThreshknot';
+import EternaFold from 'eterna/folding/Eternafold';
 
 function CreateFolder(type: any): Promise<Folder | null> {
     return type.create();
@@ -15,13 +16,15 @@ test(`EternaFoldThreshknot:folds_structures`, () => {
         "GGGGGGGGAAAACGGAAAGCCACCCCCC",
         "CCCAGGUCGUAGAACUAAAGCGCCAAAAGGACGCAAUUAGAACUACAUGCAUAGCAUGACCAAAGGG",
         "CCUACUAGGGGAGCCAAAAGGCUGAGAUGAAUGUAUUCAGACCCUUAUAACCUGAUUUGGUUAAUACCAACGUAGGAAAGUAGUUAUUAACUAUUCGUCAUUGAGAUGUCUUGGUCUAACUACUUUCUUCGCUGGGAAGUAGUU",
+        "GGGAACGACUCGAGUAGAGUCGAAAAAUCUUCAUAACUGAAUACAAAGGUAGAAGAUUAUUCAGUUAUAUUAAAAAGGAUGUCUUCGGGCAUUCAAAAGAAACAACAACAACAAC"
     ];
     const testStructures = [
         "......",
         "(((((......)))))",
         "((((((((...........)).))))))",
         "(((.((((((((..((((.(((((....)).)))..))))..))))((((...))))))))...)))",
-        ".(((((((((.((((....))))....((((....))))..))))..(((((......)))))...(((.((.(((((((((((((...((((..((((.....))))...)))).))))))))))))))).)))..))))).."
+        ".(((((((((.((((....))))....((((....))))..))))..(((((......)))))...(((.((.(((((((((((((...((((..((((.....))))...)))).))))))))))))))).)))..)))))..",
+        ".....((((((.....))))))....(((((({{{{{{{{{{{........))))))}}}}}}}}}}}........(((((((....)))))))....................."
     ];
 
     return expect(CreateFolder(EternaFoldThreshknot)
@@ -29,7 +32,8 @@ test(`EternaFoldThreshknot:folds_structures`, () => {
             if (folder === null) return;
 
             testSequences.map((sequence, index) => {
-                const foldedStructure =  folder.foldSequence(Sequence.fromSequenceString(sequence), new SecStruct());
+                const foldedStructure =  folder.foldSequence(Sequence.fromSequenceString(sequence), new SecStruct(), null, true);
+                // console.log(foldedStructure?.getParenthesis(null, true))
                 expect(foldedStructure?.getParenthesis(null, true)).toBe(testStructures[index])
             })
         })).resolves.toBeUndefined();
@@ -43,13 +47,68 @@ test(`EternaFoldThreshknot:predicts_pseudoknots`, () => {
         "GGGAACGACUCGAGUAGAGUCGAAAAGCAUCGAGCUAGCAAGAGCAUGUUGGAAGCUAGCAACCUUCAUGCCGAUGCAAUCCCAGGCUAAUGCCAACGACUCUACUCGAGUCGACAGCUUUGUUUCGACAAAGCAAAAGAAACAACAACAACAAC"
     ];
 
+    const testStructures = [
+        "((((((((((((({{{{{{)))...((((((((((((((((}}}}}}))).)))))))))))))))))))))))..(((((((....))))))).....................",
+        ".....((((((.....)))))).............(((((.................{{{{......)))))}}}}(((((((....))))))).....................",
+        ".....((((((.....))))))....(((((........)))))..((((.....((((..((....................{{{{{{{..))..))))..))))}}}}}}}...(((((((....))))))).....................",
+        ".....(((((((((((((((((....(((((({{{(({{{...{{{{{..{{....))....}}..}}}}})).}}}........}}}.))))...)))))))))))))))))...(((((((....)))))))....................."
+    ];
+
     return expect(CreateFolder(EternaFoldThreshknot)
         .then((folder) => {
             if (folder === null) return;
 
-            testSequences.map((sequence) => {
-                const foldedStructure =  folder.foldSequence(Sequence.fromSequenceString(sequence), new SecStruct());
-                expect(foldedStructure?.getParenthesis(null, true)).toContain("{")
+            testSequences.map((sequence, index) => {
+                const foldedStructure =  folder.foldSequence(Sequence.fromSequenceString(sequence), new SecStruct(), null, true);
+                // console.log(foldedStructure?.getParenthesis(null, true))
+                expect(foldedStructure?.getParenthesis(null, true)).toBe(testStructures[index])
             })
         })).resolves.toBeUndefined();
 });
+
+test('EternaFoldThreshknot:matches_Eternafold_w/o_pseudoknots', () => {    
+    
+    const testSequences = [
+        "GGGAACGACUCGAGUAGAGUCGAAAAGAAUACGUUGGGAAACGCCUUCGAACGAUCCCACGCGAUCGUCUGUUCGAGGUAAUUUUCGAAUUACCAAAAGAAACAACAACAACAAC",
+        "GGGAACGACUCGAGUAGAGUCGAAAAGAAAGCGUGCGGACGAACGUGCGUGCGUGCGAACGGACGUGCGUAAGCAACGUUAUAUUCGUAUAACGAAAAGAAACAACAACAACAAC"
+    ];
+
+    return expect(CreateFolder(EternaFoldThreshknot)
+        .then((folderTK) => {
+            CreateFolder(EternaFold).then((folderEF) => {
+                if (folderTK === null) return;
+                if (folderEF === null) return;
+
+                testSequences.map((sequence) => {
+                    const foldedStructureTK =  folderTK.foldSequence(Sequence.fromSequenceString(sequence), new SecStruct(), null, false);
+                    const foldedStructureEF =  folderEF.foldSequence(Sequence.fromSequenceString(sequence), new SecStruct());
+                    const dbnTK = foldedStructureTK?.getParenthesis(null, false);
+                    const dbnEF = foldedStructureEF?.getParenthesis(null, false);
+                    expect(dbnTK).toBe(dbnEF)
+                })
+            })
+        })).resolves.toBeUndefined();
+})  
+
+test('EternaFoldThreshknot:removes_single_pair_helices', () => {
+// This sequence is predicted to be (((.(.((....)).).))) by Eternafold
+// This test checks that Threshknot removes the single pair helix
+    
+const testSequences = [
+    "CGGAGACCAAAAGGACACCG"
+];
+
+const testStructures = [
+    "(((...((....))...)))"
+];
+
+ return expect(CreateFolder(EternaFoldThreshknot)
+    .then((folder) => {
+        if (folder === null) return;
+
+        testSequences.map((sequence, index) => {
+            const foldedStructure =  folder.foldSequence(Sequence.fromSequenceString(sequence), new SecStruct(), null, true);
+            expect(foldedStructure?.getParenthesis(null, true)).toBe(testStructures[index])
+        })
+    })).resolves.toBeUndefined();
+})
