8f94bb11c55a1719286b6facd932349e65046286
Jonathan Romano <jonathan@luxaritas.com>
2023-04-13 19:31:54 -0400

    Grammar

--- src/eterna/ui/ConfirmTargetDialog.ts
+++ src/eterna/ui/ConfirmTargetDialog.ts
@@ -17,7 +17,7 @@ export default class ConfirmTargetDialog extends WindowDialog<'cancel' | 'reset'
         const PROMPT = 'This design has a custom target structure that will be uploaded along with your design, '
          + 'but there are paired bases in the target structure with base types that are not valid pairs (AU/UG/GC).\n\n'
          + 'You can do one of the following:\n'
-         + '• Cancel submitting and make further modifications\n'
+         + '• Cancel submission and make further modifications\n'
          + '• Reset the target structure to the puzzle\'s default\n'
          + '• Continue submitting this design anyways';
         const text = Fonts.std(PROMPT, 15).color(0xC0DCE7).wordWrap(true, 300).build();
