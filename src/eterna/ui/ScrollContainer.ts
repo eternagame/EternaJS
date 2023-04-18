eternagame/EternaJS

commit f7ebcddb221a5d25bb65a80cc72585506e48f485

Fix touchscreen interactions with potentially-scrollable regions (#697)
## Summary
Currently, at least in certain situations, interactions with items in
potentially-scrollable regions do not properly fire interaction events.

## Implementation Notes
My guess is that something in the new event system changed behavior.
Regardless, my initial comment about "touch events don't have a
location" was wrong, and this handles them appropriately (or at least, I
hope good enough - may want to do more testing with multitouch at some
point).

## Testing
Clicking the submit button in the Chrome mobile emulator with touch
emulation now works

src/eterna/ui/ScrollContainer.ts
@@ -74,9 +74,6 @@ export default class ScrollContainer extends ContainerObject {
         earlyHandlers.push(this._boundHME);
         earlyTouchHandlers.push(this._boundHTE);
         Assert.assertIsDefined(Flashbang.app.pixi);
-        // @ts-expect-error This is readonly, but we have to disable this to force Pixi
-        // to only use pointer events - see handleTouchEvent
-        if (Flashbang.pixi) Flashbang.pixi.renderer.events.supportsTouchEvents = false;
     }
 
     protected dispose(): void {
@@ -221,19 +218,44 @@ export default class ScrollContainer extends ContainerObject {
     }
 
     /**
-     * Similar to handlePossiblyMaskedEvent. However, touch events do not have a position, only a
-     * target element, so we can't filter it like we do there. Luckily touch events aren't
-     * *actually* required for PIXI to function properly (they really should be just used as
-     * fallbacks when PointerEvents aren't available, and we expect them to be available anyways),
-     * so we can just make sure these events never get to Pixi so that it doesn't erroneously
-     * handle these events
+     * Similar to handlePossiblyMaskedEvent.
      *
      * @param e Touch event to be handled
      */
     private handleTouchEvent(e: TouchEvent): void {
-        if (e.target === Flashbang.app.view) {
+        const {
+            x, y, width, height
+        } = this.getBounds();
+
+        const isMaskedElement = this._htmlWrapper.contains(e.target as HTMLDivElement);
+
+        if (
+            isMaskedElement
+            && (
+                e.touches[0].clientX < x
+                || e.touches[0].clientX > x + width
+                || e.touches[0].clientY < y
+                || e.touches[0].clientY > y + height
+            )
+        ) {
+            // This event was fired on a location outside our clip path, and because
+            // we register this callback for elements within our scroll panel, this must mean
+            // it should have been clipped.
             e.preventDefault();
             e.stopPropagation();
+
+            // Find the next element "under" this element that isn't in our scroll container,
+            // to re-fire the event on.
+            const candidateEls = document.elementsFromPoint(e.touches[0].clientX, e.touches[0].clientY);
+            const newTarget = candidateEls.find((el) => !this._htmlWrapper.contains(el));
+            if (!newTarget) return;
+
+            newTarget.dispatchEvent(new TouchEvent(e.type, {
+                ...e,
+                touches: [...e.touches],
+                changedTouches: [...e.changedTouches],
+                targetTouches: [...e.targetTouches]
+            }));
         }
     }
 

src/eterna/debug/TestMode.ts
@@ -1,36 +1,7 @@
 import GameMode from 'eterna/mode/GameMode';
-import PuzzleManager from 'eterna/puzzle/PuzzleManager';
-import Puzzle from 'eterna/puzzle/Puzzle';
-import SolutionManager from 'eterna/puzzle/SolutionManager';
-import Solution from 'eterna/puzzle/Solution';
-import ViewSolutionOverlay from 'eterna/mode/DesignBrowser/ViewSolutionOverlay';
 
 export default class TestMode extends GameMode {
     protected setup(): void {
         super.setup();
-
-        const puzzleID = 7656242;
-        const loadPuzzle = PuzzleManager.instance.getPuzzleByID(puzzleID);
-        const loadSolutions = SolutionManager.instance.getSolutionsForPuzzle(puzzleID);
-        Promise.all([loadPuzzle, loadSolutions])
-            .then(([puzzle, solutions]) => {
-                this.showActionBox(puzzle, solutions[0]);
-            });
-    }
-
-    private showActionBox(puzzle: Puzzle, solution: Solution): void {
-        const actionBox = new ViewSolutionOverlay({
-            solution,
-            puzzle,
-            voteDisabled: false,
-            onPrevious: () => {},
-            onNext: () => {},
-            parentMode: (() => this)()
-        });
-        this.addObject(actionBox, this.dialogLayer);
-    }
-
-    public onContextMenuEvent(e: Event): void {
-        e.preventDefault();
     }
 }

commit c4322c83ef6212c50132a816eb55f48012d5edd9

Fix errors interacting in scrollable regions on touch devices (#698)
## Summary
A player reported the following issue on mobile

![image](https://user-images.githubusercontent.com/18635705/232848396-08bbc877-a263-4e65-8561-c7d8627a8ede.png)

This resolves that error

## Implementation Notes
Not only is `touches` not always available on `touchend`, but we always
want to use `changedTouches`, as that is both always defined and
correlates to the actual pointer which is triggering the event, which is
what we should be filtering on

## Testing
Clicking/dragging/releasing over buttons, inputs, and background in
submit dialog, masked overlapping elements in end screen dialog. Tested
with firefox touch emulation

## Related Issues
This addresses issues introduced in #697

src/eterna/ui/ScrollContainer.ts
@@ -229,13 +229,14 @@ export default class ScrollContainer extends ContainerObject {
 
         const isMaskedElement = this._htmlWrapper.contains(e.target as HTMLDivElement);
 
+        const touch = e.changedTouches[0];
         if (
             isMaskedElement
             && (
-                e.touches[0].clientX < x
-                || e.touches[0].clientX > x + width
-                || e.touches[0].clientY < y
-                || e.touches[0].clientY > y + height
+                touch.clientX < x
+                || touch.clientX > x + width
+                || touch.clientY < y
+                || touch.clientY > y + height
             )
         ) {
             // This event was fired on a location outside our clip path, and because
@@ -246,7 +247,7 @@ export default class ScrollContainer extends ContainerObject {
 
             // Find the next element "under" this element that isn't in our scroll container,
             // to re-fire the event on.
-            const candidateEls = document.elementsFromPoint(e.touches[0].clientX, e.touches[0].clientY);
+            const candidateEls = document.elementsFromPoint(touch.clientX, touch.clientY);
             const newTarget = candidateEls.find((el) => !this._htmlWrapper.contains(el));
             if (!newTarget) return;
 