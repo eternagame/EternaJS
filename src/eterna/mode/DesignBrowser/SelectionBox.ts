d24d618795393d61a8648c5d3ceb44e63ad7bba6
Jonathan Romano <jonathan@luxaritas.com>
2023-07-14 12:25:01 -0400

    Fix design browser selection on subsequent visits

--- src/eterna/mode/DesignBrowser/SelectionBox.ts
+++ src/eterna/mode/DesignBrowser/SelectionBox.ts
@@ -1,9 +1,10 @@
-import {Graphics} from 'pixi.js';
+import {Graphics, Rectangle} from 'pixi.js';
 
 export default class SelectionBox extends Graphics {
     constructor(color: number) {
         super();
         this._color = color;
+        this.hitArea = new Rectangle();
     }
 
     public setSize(width: number, height: number): void {
