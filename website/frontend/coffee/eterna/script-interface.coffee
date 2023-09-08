eternagame/EternaJS

commit e2c21198ba6c1e7cc2a65f4e020d6ba54ecb4c3c

Improve efficiency of Eternascript out pervasive

website/frontend/coffee/eterna/script-interface.coffee
@@ -40,8 +40,7 @@
 
   out : (result) ->
     $result = $('#result')
-    value = $result.get(0).innerHTML
-    $result.html(value + result)
+    $result.append(result)
     
   outln : (result) ->
     @out result

commit da8186b9f2485e6a4c2f6abce46dcaecccdab6d5

Re-apply EternaScript fixes for error handling and out performance
Originally applied in 4689ca8feea8a4005e01a6a0b1c833b04c8b9c96 and
e2c21198ba6c1e7cc2a65f4e020d6ba54ecb4c3c, accidentally reverted in f0493528b26b033d0facf9a1bf614c9a087dc244

website/frontend/coffee/eterna/script-interface.coffee
@@ -40,8 +40,7 @@
 
   out : (result) ->
     $result = $('#result')
-    value = $result.get(0).innerHTML
-    $result.html(value + result)
+    $result.append(result)
     
   outln : (result) ->
     @out result
@@ -137,8 +136,8 @@
       ret['result'] = true
       ret['eval_time'] = (new Date()).getTime() - _global_timer.getTime();
       return ret
-    catch Error
-      ret['cause'] = Error.message
+    catch err
+      ret['cause'] = err.message
       ret['result'] = false
       ret['eval_time'] = (new Date()).getTime() - _global_timer.getTime();
       return ret