228cd424ecb6d25d6d7a06ca35fc707818526a6f
guyguy2001 <guygeva2311@gmail.com>
2024-06-14 17:53:26 +0300

    Create folding-api.html, which exposes the API family of applet.fold

---
+++ src/eterna/FoldingAPIApp.ts
@@ -0,0 +1,119 @@
+import * as log from 'loglevel';
+import ExternalInterface, {ExternalInterfaceCtx} from './util/ExternalInterface';
+import Vienna from './folding/Vienna';
+import Vienna2 from './folding/Vienna2';
+import NuPACK from './folding/NuPACK';
+import Contrafold from './folding/Contrafold';
+import EternaFold from './folding/Eternafold';
+import EternaFoldThreshknot from './folding/EternafoldThreshknot';
+import RNAFoldBasic from './folding/RNAFoldBasic';
+import FolderManager from './folding/FolderManager';
+import LinearFoldC from './folding/LinearFoldC';
+import LinearFoldE from './folding/LinearFoldE';
+import LinearFoldV from './folding/LinearFoldV';
+import Folder from './folding/Folder';
+import FoldingAPI from './eternaScript/FoldingAPI';
+
+interface FoldingAppParams {
+    containerID?: string;
+    folderName?: string;
+}
+
+interface ProcessedFoldingAppParams {
+    containerID: string;
+    folderName: string;
+}
+
+export class WasmNotSupportedError extends Error {}
+
+export class ContainerElementNotFound extends Error {}
+
+/**
+ * Entry point for the folding API provider.
+ *
+ * This is an alternate version of EternaJS, only exposing the API needed for scripts to work
+ * (e.g. `Lib.fold` via `document.getElementById("maingame").fold`).
+ * */
+export default class FoldingAPIApp {
+    constructor(params: FoldingAppParams) {
+        // Default param values
+        params.containerID = params.containerID || 'maingame';
+        params.folderName = 'vienna';
+
+        this._params = {containerID: params.containerID, folderName: params.folderName};
+
+        const appContainer: HTMLElement | null = document.getElementById(params.containerID);
+        if (!appContainer) {
+            throw new ContainerElementNotFound(`Could not find HTML element with ID ${params.containerID}`);
+        }
+        this._appContainer = appContainer;
+
+        ExternalInterface.init(appContainer);
+    }
+
+    private static isWebAssemblySupported() {
+        return typeof WebAssembly === 'object';
+    }
+
+    public async run(): Promise<void> {
+        if (!FoldingAPIApp.isWebAssemblySupported()) {
+            throw new WasmNotSupportedError(
+                "Can't initialize the folding API app, since the browser doesn't support WASM"
+            );
+        }
+
+        await this.initFoldingEngines();
+        this.initScriptInterface();
+    }
+
+    public disposeNow(): void {
+        this._appContainer.innerHTML = '';
+
+        FolderManager.dispose();
+        ExternalInterface.dispose();
+    }
+
+    private async initFoldingEngines(): Promise<void> {
+        log.info('Initializing folding engines...');
+        const folders: (Folder | null)[] = await Promise.all([
+            Vienna.create(),
+            Vienna2.create(),
+            NuPACK.create(),
+            LinearFoldC.create(),
+            LinearFoldE.create(),
+            LinearFoldV.create(),
+            Contrafold.create(),
+            EternaFold.create(),
+            EternaFoldThreshknot.create(),
+            RNAFoldBasic.create()]);
+
+        log.info('Folding engines intialized');
+        for (const folder of folders) {
+            if (folder !== null) {
+                FolderManager.instance.addFolder(folder);
+            }
+        }
+
+        const folder = FolderManager.instance.getFolder(this._params.folderName);
+        if (folder === null) {
+            log.warn(`No such folder '${this._params.folderName}'`);
+        } else {
+            this._folder = folder;
+        }
+    }
+
+    private initScriptInterface(): void {
+        new FoldingAPI({
+            getFolder: () => this._folder,
+            getIsPseudoknot: () => false
+        }).registerToScriptInterface(this._scriptInterface);
+
+        ExternalInterface.pushContext(this._scriptInterface);
+    }
+
+    private readonly _params: ProcessedFoldingAppParams;
+    private readonly _scriptInterface: ExternalInterfaceCtx = new ExternalInterfaceCtx();
+    private readonly _appContainer: HTMLElement;
+    // private readonly _folderSwitcher: FolderSwitcher;
+    private _folder: Folder;
+}
--- src/eterna/index.ts
+++ src/eterna/index.ts
@@ -1,5 +1,6 @@
 import * as log from 'loglevel';
 import EternaApp from 'eterna/EternaApp';
+import FoldingAPIApp from 'eterna/FoldingAPIApp';
 import * as PIXI from 'pixi.js';
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -8,9 +9,11 @@ log.setLevel(isProduction ? 'info' : 'trace');
 declare global {
     interface Window {
         EternaApp: typeof EternaApp;
+        FoldingAPIApp: typeof FoldingAPIApp;
         app: EternaApp; // this syntax is used in index.html.tmpl, at least...
         __PIXI_APP__?: PIXI.Application;
     }
 }
 
 window.EternaApp = EternaApp;
+window.FoldingAPIApp = FoldingAPIApp;
