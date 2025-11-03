eeee03714654336fd393ce579ab00a905fc2a8eb
Jonathan Romano <jonathan@luxaritas.com>
2025-11-03 17:58:05 -0500

    Reposition barcode region in reverse-complement states

--- src/eterna/puzzle/__tests__/Puzzle.test.ts
+++ src/eterna/puzzle/__tests__/Puzzle.test.ts
@@ -14,7 +14,7 @@ describe('Puzzle#barcodeIndices', () => {
         puz.useBarcode = true;
         puz.barcodeStart = 10;
         puz.secstructs = ['.'.repeat(50)];
-        expect(puz.barcodeIndices).toEqual([10, 11, 12, 13, 14, 15, 16]);
+        expect(puz.getBarcodeIndices(0)).toEqual([10, 11, 12, 13, 14, 15, 16]);
     });
 
     it('should return the 7 bases prior to the tail if tails are enabled', () => {
@@ -24,14 +24,14 @@ describe('Puzzle#barcodeIndices', () => {
         // There will be 5 extra bases added to the front and 20 added to the end,
         // so our puzzle will actually be 75 bases long instead of 50
         puz.setUseTails(true, false);
-        expect(puz.barcodeIndices).toEqual([47, 48, 49, 50, 51, 52, 53]);
+        expect(puz.getBarcodeIndices(0)).toEqual([47, 48, 49, 50, 51, 52, 53]);
     });
 
     it('should return the last 7 bases if neither barcode start or tails are specified', () => {
         const puz = new Puzzle(0, 'puzzle', PuzzleType.EXPERIMENTAL, 'author');
         puz.useBarcode = true;
         puz.secstructs = ['.'.repeat(50)];
-        expect(puz.barcodeIndices).toEqual([43, 44, 45, 46, 47, 48, 49]);
+        expect(puz.getBarcodeIndices(0)).toEqual([43, 44, 45, 46, 47, 48, 49]);
     });
 
     it('should prefer the barcode start field over the use tails value', () => {
@@ -40,7 +40,7 @@ describe('Puzzle#barcodeIndices', () => {
         puz.barcodeStart = 10;
         puz.secstructs = ['.'.repeat(50)];
         puz.setUseTails(true, false);
-        expect(puz.barcodeIndices).toEqual([10, 11, 12, 13, 14, 15, 16]);
+        expect(puz.getBarcodeIndices(0)).toEqual([10, 11, 12, 13, 14, 15, 16]);
     });
 });
 
